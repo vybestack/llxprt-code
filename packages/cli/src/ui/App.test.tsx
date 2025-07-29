helldrum/gemini-cli-no-sec

feat: Add auto update functionality (#4686)

packages/cli/src/ui/App.test.tsx
@@ -23,6 +23,9 @@ import { useGeminiStream } from './hooks/useGeminiStream.js';
 import { useConsoleMessages } from './hooks/useConsoleMessages.js';
 import { StreamingState, ConsoleMessageItem } from './types.js';
 import { Tips } from './components/Tips.js';
+import { checkForUpdates, UpdateObject } from './utils/updateCheck.js';
+import { EventEmitter } from 'events';
+import { updateEventEmitter } from '../utils/updateEventEmitter.js';
 
 // Define a more complete mock server config based on actual Config
 interface MockServerConfig {
@@ -163,6 +166,7 @@ vi.mock('@google/gemini-cli-core', async (importOriginal) => {
     MCPServerConfig: actualCore.MCPServerConfig,
     getAllGeminiMdFilenames: vi.fn(() => ['GEMINI.md']),
     ideContext: ideContextMock,
+    isGitRepository: vi.fn(),
   };
 });
 
@@ -219,6 +223,17 @@ vi.mock('./components/Tips.js', () => ({
 vi.mock('./components/Header.js', () => ({
   Header: vi.fn(() => null),
 }));
+
+vi.mock('./utils/updateCheck.js', () => ({
+  checkForUpdates: vi.fn(),
+}));
+
+const mockedCheckForUpdates = vi.mocked(checkForUpdates);
+const { isGitRepository: mockedIsGitRepository } = vi.mocked(
+  await import('@google/gemini-cli-core'),
+);
+
+vi.mock('node:child_process');
 
 describe('App UI', () => {
   let mockConfig: MockServerConfig;
@@ -286,6 +301,169 @@ describe('App UI', () => {
       currentUnmount = undefined;
     }
     vi.clearAllMocks(); // Clear mocks after each test
+  });
+
+  describe('handleAutoUpdate', () => {
+    let spawnEmitter: EventEmitter;
+
+    beforeEach(async () => {
+      const { spawn } = await import('node:child_process');
+      spawnEmitter = new EventEmitter();
+      spawnEmitter.stdout = new EventEmitter();
+      spawnEmitter.stderr = new EventEmitter();
+      (spawn as vi.Mock).mockReturnValue(spawnEmitter);
+    });
+
+    afterEach(() => {
+      delete process.env.GEMINI_CLI_DISABLE_AUTOUPDATER;
+    });
+
+    it('should not start the update process when running from git', async () => {
+      mockedIsGitRepository.mockResolvedValue(true);
+      const info: UpdateObject = {
+        update: {
+          name: '@google/gemini-cli',
+          latest: '1.1.0',
+          current: '1.0.0',
+        },
+        message: 'Gemini CLI update available!',
+      };
+      mockedCheckForUpdates.mockResolvedValue(info);
+      const { spawn } = await import('node:child_process');
+
+      const { unmount } = render(
+        <App
+          config={mockConfig as unknown as ServerConfig}
+          settings={mockSettings}
+          version={mockVersion}
+        />,
+      );
+      currentUnmount = unmount;
+
+      await new Promise((resolve) => setTimeout(resolve, 10));
+
+      expect(spawn).not.toHaveBeenCalled();
+    });
+
+    it('should show a success message when update succeeds', async () => {
+      mockedIsGitRepository.mockResolvedValue(false);
+      const info: UpdateObject = {
+        update: {
+          name: '@google/gemini-cli',
+          latest: '1.1.0',
+          current: '1.0.0',
+        },
+        message: 'Update available',
+      };
+      mockedCheckForUpdates.mockResolvedValue(info);
+
+      const { lastFrame, unmount } = render(
+        <App
+          config={mockConfig as unknown as ServerConfig}
+          settings={mockSettings}
+          version={mockVersion}
+        />,
+      );
+      currentUnmount = unmount;
+
+      updateEventEmitter.emit('update-success', info);
+
+      await new Promise((resolve) => setTimeout(resolve, 10));
+
+      expect(lastFrame()).toContain(
+        'Update successful! The new version will be used on your next run.',
+      );
+    });
+
+    it('should show an error message when update fails', async () => {
+      mockedIsGitRepository.mockResolvedValue(false);
+      const info: UpdateObject = {
+        update: {
+          name: '@google/gemini-cli',
+          latest: '1.1.0',
+          current: '1.0.0',
+        },
+        message: 'Update available',
+      };
+      mockedCheckForUpdates.mockResolvedValue(info);
+
+      const { lastFrame, unmount } = render(
+        <App
+          config={mockConfig as unknown as ServerConfig}
+          settings={mockSettings}
+          version={mockVersion}
+        />,
+      );
+      currentUnmount = unmount;
+
+      updateEventEmitter.emit('update-failed', info);
+
+      await new Promise((resolve) => setTimeout(resolve, 10));
+
+      expect(lastFrame()).toContain(
+        'Automatic update failed. Please try updating manually',
+      );
+    });
+
+    it('should show an error message when spawn fails', async () => {
+      mockedIsGitRepository.mockResolvedValue(false);
+      const info: UpdateObject = {
+        update: {
+          name: '@google/gemini-cli',
+          latest: '1.1.0',
+          current: '1.0.0',
+        },
+        message: 'Update available',
+      };
+      mockedCheckForUpdates.mockResolvedValue(info);
+
+      const { lastFrame, unmount } = render(
+        <App
+          config={mockConfig as unknown as ServerConfig}
+          settings={mockSettings}
+          version={mockVersion}
+        />,
+      );
+      currentUnmount = unmount;
+
+      // We are testing the App's reaction to an `update-failed` event,
+      // which is what should be emitted when a spawn error occurs elsewhere.
+      updateEventEmitter.emit('update-failed', info);
+
+      await new Promise((resolve) => setTimeout(resolve, 10));
+
+      expect(lastFrame()).toContain(
+        'Automatic update failed. Please try updating manually',
+      );
+    });
+
+    it('should not auto-update if GEMINI_CLI_DISABLE_AUTOUPDATER is true', async () => {
+      mockedIsGitRepository.mockResolvedValue(false);
+      process.env.GEMINI_CLI_DISABLE_AUTOUPDATER = 'true';
+      const info: UpdateObject = {
+        update: {
+          name: '@google/gemini-cli',
+          latest: '1.1.0',
+          current: '1.0.0',
+        },
+        message: 'Update available',
+      };
+      mockedCheckForUpdates.mockResolvedValue(info);
+      const { spawn } = await import('node:child_process');
+
+      const { unmount } = render(
+        <App
+          config={mockConfig as unknown as ServerConfig}
+          settings={mockSettings}
+          version={mockVersion}
+        />,
+      );
+      currentUnmount = unmount;
+
+      await new Promise((resolve) => setTimeout(resolve, 10));
+
+      expect(spawn).not.toHaveBeenCalled();
+    });
   });
 
   it('should display active file when available', async () => {

packages/cli/src/ui/utils/updateCheck.ts
@@ -4,11 +4,16 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import updateNotifier from 'update-notifier';
+import updateNotifier, { UpdateInfo } from 'update-notifier';
 import semver from 'semver';
 import { getPackageJson } from '../../utils/package.js';
 
-export async function checkForUpdates(): Promise<string | null> {
+export interface UpdateObject {
+  message: string;
+  update: UpdateInfo;
+}
+
+export async function checkForUpdates(): Promise<UpdateObject | null> {
   try {
     // Skip update check when running from source (development mode)
     if (process.env.DEV === 'true') {
@@ -30,11 +35,13 @@ export async function checkForUpdates(): Promise<string | null> {
       shouldNotifyInNpmScript: true,
     });
 
-    if (
-      notifier.update &&
-      semver.gt(notifier.update.latest, notifier.update.current)
-    ) {
-      return `Gemini CLI update available! ${notifier.update.current} → ${notifier.update.latest}\nRun npm install -g ${packageJson.name} to update`;
+    const updateInfo = await notifier.fetchInfo();
+
+    if (updateInfo && semver.gt(updateInfo.latest, updateInfo.current)) {
+      return {
+        message: `Gemini CLI update available! ${updateInfo.current} → ${updateInfo.latest}`,
+        update: updateInfo,
+      };
     }
 
     return null;

packages/cli/src/utils/installationInfo.ts
@@ -1,0 +1,177 @@
+/**
+ * @license
+ * Copyright 2025 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { isGitRepository } from '@google/gemini-cli-core';
+import * as fs from 'fs';
+import * as path from 'path';
+import * as childProcess from 'child_process';
+
+export enum PackageManager {
+  NPM = 'npm',
+  YARN = 'yarn',
+  PNPM = 'pnpm',
+  PNPX = 'pnpx',
+  BUN = 'bun',
+  BUNX = 'bunx',
+  HOMEBREW = 'homebrew',
+  NPX = 'npx',
+  UNKNOWN = 'unknown',
+}
+
+export interface InstallationInfo {
+  packageManager: PackageManager;
+  isGlobal: boolean;
+  updateCommand?: string;
+  updateMessage?: string;
+}
+
+export function getInstallationInfo(
+  projectRoot: string,
+  isAutoUpdateDisabled: boolean,
+): InstallationInfo {
+  const cliPath = process.argv[1];
+  if (!cliPath) {
+    return { packageManager: PackageManager.UNKNOWN, isGlobal: false };
+  }
+
+  try {
+    // Normalize path separators to forward slashes for consistent matching.
+    const realPath = fs.realpathSync(cliPath).replace(/\\/g, '/');
+    const normalizedProjectRoot = projectRoot?.replace(/\\/g, '/');
+    const isGit = isGitRepository(process.cwd());
+
+    // Check for local git clone first
+    if (
+      isGit &&
+      normalizedProjectRoot &&
+      realPath.startsWith(normalizedProjectRoot) &&
+      !realPath.includes('/node_modules/')
+    ) {
+      return {
+        packageManager: PackageManager.UNKNOWN, // Not managed by a package manager in this sense
+        isGlobal: false,
+        updateMessage:
+          'Running from a local git clone. Please update with "git pull".',
+      };
+    }
+
+    // Check for npx/pnpx
+    if (realPath.includes('/.npm/_npx') || realPath.includes('/npm/_npx')) {
+      return {
+        packageManager: PackageManager.NPX,
+        isGlobal: false,
+        updateMessage: 'Running via npx, update not applicable.',
+      };
+    }
+    if (realPath.includes('/.pnpm/_pnpx')) {
+      return {
+        packageManager: PackageManager.PNPX,
+        isGlobal: false,
+        updateMessage: 'Running via pnpx, update not applicable.',
+      };
+    }
+
+    // Check for Homebrew
+    if (process.platform === 'darwin') {
+      try {
+        // The package name in homebrew is gemini-cli
+        childProcess.execSync('brew list -1 | grep -q "^gemini-cli$"', {
+          stdio: 'ignore',
+        });
+        return {
+          packageManager: PackageManager.HOMEBREW,
+          isGlobal: true,
+          updateMessage:
+            'Installed via Homebrew. Please update with "brew upgrade".',
+        };
+      } catch (_error) {
+        // Brew is not installed or gemini-cli is not installed via brew.
+        // Continue to the next check.
+      }
+    }
+
+    // Check for pnpm
+    if (realPath.includes('/.pnpm/global')) {
+      const updateCommand = 'pnpm add -g @google/gemini-cli@latest';
+      return {
+        packageManager: PackageManager.PNPM,
+        isGlobal: true,
+        updateCommand,
+        updateMessage: isAutoUpdateDisabled
+          ? `Please run ${updateCommand} to update`
+          : 'Installed with pnpm. Attempting to automatically update now...',
+      };
+    }
+
+    // Check for yarn
+    if (realPath.includes('/.yarn/global')) {
+      const updateCommand = 'yarn global add @google/gemini-cli@latest';
+      return {
+        packageManager: PackageManager.YARN,
+        isGlobal: true,
+        updateCommand,
+        updateMessage: isAutoUpdateDisabled
+          ? `Please run ${updateCommand} to update`
+          : 'Installed with yarn. Attempting to automatically update now...',
+      };
+    }
+
+    // Check for bun
+    if (realPath.includes('/.bun/install/cache')) {
+      return {
+        packageManager: PackageManager.BUNX,
+        isGlobal: false,
+        updateMessage: 'Running via bunx, update not applicable.',
+      };
+    }
+    if (realPath.includes('/.bun/bin')) {
+      const updateCommand = 'bun add -g @google/gemini-cli@latest';
+      return {
+        packageManager: PackageManager.BUN,
+        isGlobal: true,
+        updateCommand,
+        updateMessage: isAutoUpdateDisabled
+          ? `Please run ${updateCommand} to update`
+          : 'Installed with bun. Attempting to automatically update now...',
+      };
+    }
+
+    // Check for local install
+    if (
+      normalizedProjectRoot &&
+      realPath.startsWith(`${normalizedProjectRoot}/node_modules`)
+    ) {
+      let pm = PackageManager.NPM;
+      if (fs.existsSync(path.join(projectRoot, 'yarn.lock'))) {
+        pm = PackageManager.YARN;
+      } else if (fs.existsSync(path.join(projectRoot, 'pnpm-lock.yaml'))) {
+        pm = PackageManager.PNPM;
+      } else if (fs.existsSync(path.join(projectRoot, 'bun.lockb'))) {
+        pm = PackageManager.BUN;
+      }
+      return {
+        packageManager: pm,
+        isGlobal: false,
+        updateMessage:
+          "Locally installed. Please update via your project's package.json.",
+      };
+    }
+
+    // Assume global npm
+    const updateCommand = 'npm install -g @google/gemini-cli@latest';
+    return {
+      packageManager: PackageManager.NPM,
+      isGlobal: true,
+      updateCommand,
+      updateMessage: isAutoUpdateDisabled
+        ? `Please run ${updateCommand} to update`
+        : 'Installed with npm. Attempting to automatically update now...',
+    };
+  } catch (error) {
+    console.log(error);
+    return { packageManager: PackageManager.UNKNOWN, isGlobal: false };
+  }
+}

vybestack/llxprt-code

packages/cli/src/ui/App.test.tsx
@@ -23,6 +23,9 @@ import { useGeminiStream } from './hooks/useGeminiStream.js';
 import { useConsoleMessages } from './hooks/useConsoleMessages.js';
 import { StreamingState, ConsoleMessageItem } from './types.js';
 import { Tips } from './components/Tips.js';
+import { checkForUpdates, UpdateObject } from './utils/updateCheck.js';
+import { EventEmitter } from 'events';
+import { updateEventEmitter } from '../utils/updateEventEmitter.js';
 
 // Define a more complete mock server config based on actual Config
 interface MockServerConfig {
@@ -163,6 +166,7 @@ vi.mock('@vybestack/llxprt-code-core', async (importOriginal) => {
     MCPServerConfig: actualCore.MCPServerConfig,
     getAllLlxprtMdFilenames: vi.fn(() => ['LLXPRT.md']),
     ideContext: ideContextMock,
+    isGitRepository: vi.fn(),
   };
 });
 
@@ -242,6 +246,17 @@ vi.mock('../providers/providerManagerInstance.js', () => ({
   })),
 }));
 
+vi.mock('./utils/updateCheck.js', () => ({
+  checkForUpdates: vi.fn(),
+}));
+
+const mockedCheckForUpdates = vi.mocked(checkForUpdates);
+const { isGitRepository: mockedIsGitRepository } = vi.mocked(
+  await import('@vybestack/llxprt-code-core'),
+);
+
+vi.mock('node:child_process');
+
 describe('App UI', () => {
   let mockConfig: MockServerConfig;
   let mockSettings: LoadedSettings;
@@ -308,6 +323,169 @@ describe('App UI', () => {
       currentUnmount = undefined;
     }
     vi.clearAllMocks(); // Clear mocks after each test
+  });
+
+  describe('handleAutoUpdate', () => {
+    let spawnEmitter: EventEmitter;
+
+    beforeEach(async () => {
+      const { spawn } = await import('node:child_process');
+      spawnEmitter = new EventEmitter();
+      spawnEmitter.stdout = new EventEmitter();
+      spawnEmitter.stderr = new EventEmitter();
+      (spawn as vi.Mock).mockReturnValue(spawnEmitter);
+    });
+
+    afterEach(() => {
+      delete process.env.GEMINI_CLI_DISABLE_AUTOUPDATER;
+    });
+
+    it('should not start the update process when running from git', async () => {
+      mockedIsGitRepository.mockResolvedValue(true);
+      const info: UpdateObject = {
+        update: {
+          name: '@vybestack/llxprt-code',
+          latest: '1.1.0',
+          current: '1.0.0',
+        },
+        message: 'LLxprt Code update available!',
+      };
+      mockedCheckForUpdates.mockResolvedValue(info);
+      const { spawn } = await import('node:child_process');
+
+      const { unmount } = render(
+        <App
+          config={mockConfig as unknown as ServerConfig}
+          settings={mockSettings}
+          version={mockVersion}
+        />,
+      );
+      currentUnmount = unmount;
+
+      await new Promise((resolve) => setTimeout(resolve, 10));
+
+      expect(spawn).not.toHaveBeenCalled();
+    });
+
+    it('should show a success message when update succeeds', async () => {
+      mockedIsGitRepository.mockResolvedValue(false);
+      const info: UpdateObject = {
+        update: {
+          name: '@vybestack/llxprt-code',
+          latest: '1.1.0',
+          current: '1.0.0',
+        },
+        message: 'Update available',
+      };
+      mockedCheckForUpdates.mockResolvedValue(info);
+
+      const { lastFrame, unmount } = render(
+        <App
+          config={mockConfig as unknown as ServerConfig}
+          settings={mockSettings}
+          version={mockVersion}
+        />,
+      );
+      currentUnmount = unmount;
+
+      updateEventEmitter.emit('update-success', info);
+
+      await new Promise((resolve) => setTimeout(resolve, 10));
+
+      expect(lastFrame()).toContain(
+        'Update successful! The new version will be used on your next run.',
+      );
+    });
+
+    it('should show an error message when update fails', async () => {
+      mockedIsGitRepository.mockResolvedValue(false);
+      const info: UpdateObject = {
+        update: {
+          name: '@vybestack/llxprt-code',
+          latest: '1.1.0',
+          current: '1.0.0',
+        },
+        message: 'Update available',
+      };
+      mockedCheckForUpdates.mockResolvedValue(info);
+
+      const { lastFrame, unmount } = render(
+        <App
+          config={mockConfig as unknown as ServerConfig}
+          settings={mockSettings}
+          version={mockVersion}
+        />,
+      );
+      currentUnmount = unmount;
+
+      updateEventEmitter.emit('update-failed', info);
+
+      await new Promise((resolve) => setTimeout(resolve, 10));
+
+      expect(lastFrame()).toContain(
+        'Automatic update failed. Please try updating manually',
+      );
+    });
+
+    it('should show an error message when spawn fails', async () => {
+      mockedIsGitRepository.mockResolvedValue(false);
+      const info: UpdateObject = {
+        update: {
+          name: '@vybestack/llxprt-code',
+          latest: '1.1.0',
+          current: '1.0.0',
+        },
+        message: 'Update available',
+      };
+      mockedCheckForUpdates.mockResolvedValue(info);
+
+      const { lastFrame, unmount } = render(
+        <App
+          config={mockConfig as unknown as ServerConfig}
+          settings={mockSettings}
+          version={mockVersion}
+        />,
+      );
+      currentUnmount = unmount;
+
+      // We are testing the App's reaction to an `update-failed` event,
+      // which is what should be emitted when a spawn error occurs elsewhere.
+      updateEventEmitter.emit('update-failed', info);
+
+      await new Promise((resolve) => setTimeout(resolve, 10));
+
+      expect(lastFrame()).toContain(
+        'Automatic update failed. Please try updating manually',
+      );
+    });
+
+    it('should not auto-update if GEMINI_CLI_DISABLE_AUTOUPDATER is true', async () => {
+      mockedIsGitRepository.mockResolvedValue(false);
+      process.env.GEMINI_CLI_DISABLE_AUTOUPDATER = 'true';
+      const info: UpdateObject = {
+        update: {
+          name: '@vybestack/llxprt-code',
+          latest: '1.1.0',
+          current: '1.0.0',
+        },
+        message: 'Update available',
+      };
+      mockedCheckForUpdates.mockResolvedValue(info);
+      const { spawn } = await import('node:child_process');
+
+      const { unmount } = render(
+        <App
+          config={mockConfig as unknown as ServerConfig}
+          settings={mockSettings}
+          version={mockVersion}
+        />,
+      );
+      currentUnmount = unmount;
+
+      await new Promise((resolve) => setTimeout(resolve, 10));
+
+      expect(spawn).not.toHaveBeenCalled();
+    });
   });
 
   it('should display active file when available', async () => {

packages/cli/src/ui/utils/updateCheck.ts
@@ -4,34 +4,16 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+import updateNotifier, { UpdateInfo } from 'update-notifier';
 import semver from 'semver';
 import { getPackageJson } from '../../utils/package.js';
 
-const UPDATE_CHECK_URL =
-  'https://registry.npmjs.org/@vybestack/llxprt-code/latest';
-const UPDATE_CHECK_TIMEOUT_MS = 5000;
-
-async function fetchWithTimeout(
-  url: string,
-  timeout: number,
-): Promise<Response> {
-  const controller = new AbortController();
-  const timeoutId = setTimeout(() => controller.abort(), timeout);
-
-  try {
-    const response = await fetch(url, { signal: controller.signal });
-    return response;
-  } catch (error) {
-    if (error instanceof Error && error.name === 'AbortError') {
-      throw new Error(`Request timed out after ${timeout}ms`);
-    }
-    throw error;
-  } finally {
-    clearTimeout(timeoutId);
-  }
+export interface UpdateObject {
+  message: string;
+  update: UpdateInfo;
 }
 
-export async function checkForUpdates(): Promise<string | null> {
+export async function checkForUpdates(): Promise<UpdateObject | null> {
   try {
     // Skip update check when running from source (development mode)
     if (process.env.DEV === 'true') {
@@ -43,22 +25,18 @@ export async function checkForUpdates(): Promise<string | null> {
       return null;
     }
 
-    const response = await fetchWithTimeout(
-      UPDATE_CHECK_URL,
-      UPDATE_CHECK_TIMEOUT_MS,
-    );
-    if (!response.ok) {
-      return null;
-    }
+    const notifier = updateNotifier({
+      pkg: packageJson,
+      updateCheckInterval: 0, // Check immediately
+    });
 
-    const latestPackageData = await response.json();
+    const updateInfo = await notifier.fetchInfo();
 
-    if (
-      latestPackageData &&
-      latestPackageData.version &&
-      semver.gt(latestPackageData.version, packageJson.version)
-    ) {
-      return `LLxprt Code update available! ${packageJson.version} → ${latestPackageData.version}\nRun npm install -g ${packageJson.name} to update`;
+    if (updateInfo && semver.gt(updateInfo.latest, updateInfo.current)) {
+      return {
+        message: `LLxprt Code update available! ${updateInfo.current} → ${updateInfo.latest}`,
+        update: updateInfo,
+      };
     }
 
     return null;

packages/cli/src/utils/installationInfo.ts
@@ -1,0 +1,177 @@
+/**
+ * @license
+ * Copyright 2025 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { isGitRepository } from '@vybestack/llxprt-code-core';
+import * as fs from 'fs';
+import * as path from 'path';
+import * as childProcess from 'child_process';
+
+export enum PackageManager {
+  NPM = 'npm',
+  YARN = 'yarn',
+  PNPM = 'pnpm',
+  PNPX = 'pnpx',
+  BUN = 'bun',
+  BUNX = 'bunx',
+  HOMEBREW = 'homebrew',
+  NPX = 'npx',
+  UNKNOWN = 'unknown',
+}
+
+export interface InstallationInfo {
+  packageManager: PackageManager;
+  isGlobal: boolean;
+  updateCommand?: string;
+  updateMessage?: string;
+}
+
+export function getInstallationInfo(
+  projectRoot: string,
+  isAutoUpdateDisabled: boolean,
+): InstallationInfo {
+  const cliPath = process.argv[1];
+  if (!cliPath) {
+    return { packageManager: PackageManager.UNKNOWN, isGlobal: false };
+  }
+
+  try {
+    // Normalize path separators to forward slashes for consistent matching.
+    const realPath = fs.realpathSync(cliPath).replace(/\\/g, '/');
+    const normalizedProjectRoot = projectRoot?.replace(/\\/g, '/');
+    const isGit = isGitRepository(process.cwd());
+
+    // Check for local git clone first
+    if (
+      isGit &&
+      normalizedProjectRoot &&
+      realPath.startsWith(normalizedProjectRoot) &&
+      !realPath.includes('/node_modules/')
+    ) {
+      return {
+        packageManager: PackageManager.UNKNOWN, // Not managed by a package manager in this sense
+        isGlobal: false,
+        updateMessage:
+          'Running from a local git clone. Please update with "git pull".',
+      };
+    }
+
+    // Check for npx/pnpx
+    if (realPath.includes('/.npm/_npx') || realPath.includes('/npm/_npx')) {
+      return {
+        packageManager: PackageManager.NPX,
+        isGlobal: false,
+        updateMessage: 'Running via npx, update not applicable.',
+      };
+    }
+    if (realPath.includes('/.pnpm/_pnpx')) {
+      return {
+        packageManager: PackageManager.PNPX,
+        isGlobal: false,
+        updateMessage: 'Running via pnpx, update not applicable.',
+      };
+    }
+
+    // Check for Homebrew
+    if (process.platform === 'darwin') {
+      try {
+        // The package name in homebrew is llxprt-code
+        childProcess.execSync('brew list -1 | grep -q "^llxprt-code$"', {
+          stdio: 'ignore',
+        });
+        return {
+          packageManager: PackageManager.HOMEBREW,
+          isGlobal: true,
+          updateMessage:
+            'Installed via Homebrew. Please update with "brew upgrade".',
+        };
+      } catch (_error) {
+        // Brew is not installed or gemini-cli is not installed via brew.
+        // Continue to the next check.
+      }
+    }
+
+    // Check for pnpm
+    if (realPath.includes('/.pnpm/global')) {
+      const updateCommand = 'pnpm add -g @vybestack/llxprt-code@latest';
+      return {
+        packageManager: PackageManager.PNPM,
+        isGlobal: true,
+        updateCommand,
+        updateMessage: isAutoUpdateDisabled
+          ? `Please run ${updateCommand} to update`
+          : 'Installed with pnpm. Attempting to automatically update now...',
+      };
+    }
+
+    // Check for yarn
+    if (realPath.includes('/.yarn/global')) {
+      const updateCommand = 'yarn global add @vybestack/llxprt-code@latest';
+      return {
+        packageManager: PackageManager.YARN,
+        isGlobal: true,
+        updateCommand,
+        updateMessage: isAutoUpdateDisabled
+          ? `Please run ${updateCommand} to update`
+          : 'Installed with yarn. Attempting to automatically update now...',
+      };
+    }
+
+    // Check for bun
+    if (realPath.includes('/.bun/install/cache')) {
+      return {
+        packageManager: PackageManager.BUNX,
+        isGlobal: false,
+        updateMessage: 'Running via bunx, update not applicable.',
+      };
+    }
+    if (realPath.includes('/.bun/bin')) {
+      const updateCommand = 'bun add -g @vybestack/llxprt-code@latest';
+      return {
+        packageManager: PackageManager.BUN,
+        isGlobal: true,
+        updateCommand,
+        updateMessage: isAutoUpdateDisabled
+          ? `Please run ${updateCommand} to update`
+          : 'Installed with bun. Attempting to automatically update now...',
+      };
+    }
+
+    // Check for local install
+    if (
+      normalizedProjectRoot &&
+      realPath.startsWith(`${normalizedProjectRoot}/node_modules`)
+    ) {
+      let pm = PackageManager.NPM;
+      if (fs.existsSync(path.join(projectRoot, 'yarn.lock'))) {
+        pm = PackageManager.YARN;
+      } else if (fs.existsSync(path.join(projectRoot, 'pnpm-lock.yaml'))) {
+        pm = PackageManager.PNPM;
+      } else if (fs.existsSync(path.join(projectRoot, 'bun.lockb'))) {
+        pm = PackageManager.BUN;
+      }
+      return {
+        packageManager: pm,
+        isGlobal: false,
+        updateMessage:
+          "Locally installed. Please update via your project's package.json.",
+      };
+    }
+
+    // Assume global npm
+    const updateCommand = 'npm install -g @vybestack/llxprt-code@latest';
+    return {
+      packageManager: PackageManager.NPM,
+      isGlobal: true,
+      updateCommand,
+      updateMessage: isAutoUpdateDisabled
+        ? `Please run ${updateCommand} to update`
+        : 'Installed with npm. Attempting to automatically update now...',
+    };
+  } catch (error) {
+    console.log(error);
+    return { packageManager: PackageManager.UNKNOWN, isGlobal: false };
+  }
+}