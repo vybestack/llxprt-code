helldrum/gemini-cli-no-sec

[ide-mode] Add openDiff tool to IDE MCP server (#4519)
Co-authored-by: gemini-code-assist[bot] <176961590+gemini-code-assist[bot]@users.noreply.github.com>

packages/vscode-ide-companion/src/extension.ts
@@ -6,12 +6,15 @@
 
 import * as vscode from 'vscode';
 import { IDEServer } from './ide-server.js';
+import { DiffContentProvider, DiffManager } from './diff-manager.js';
 import { createLogger } from './utils/logger.js';
 
 const IDE_WORKSPACE_PATH_ENV_VAR = 'GEMINI_CLI_IDE_WORKSPACE_PATH';
+export const DIFF_SCHEME = 'gemini-diff';
 
 let ideServer: IDEServer;
 let logger: vscode.OutputChannel;
+
 let log: (message: string) => void = () => {};
 
 function updateWorkspacePath(context: vscode.ExtensionContext) {
@@ -37,7 +40,40 @@ export async function activate(context: vscode.ExtensionContext) {
 
   updateWorkspacePath(context);
 
-  ideServer = new IDEServer(log);
+  const diffContentProvider = new DiffContentProvider();
+  const diffManager = new DiffManager(logger, diffContentProvider);
+
+  context.subscriptions.push(
+    vscode.workspace.onDidCloseTextDocument((doc) => {
+      if (doc.uri.scheme === DIFF_SCHEME) {
+        diffManager.cancelDiff(doc.uri);
+      }
+    }),
+    vscode.workspace.registerTextDocumentContentProvider(
+      DIFF_SCHEME,
+      diffContentProvider,
+    ),
+    vscode.commands.registerCommand(
+      'gemini.diff.accept',
+      (uri?: vscode.Uri) => {
+        const docUri = uri ?? vscode.window.activeTextEditor?.document.uri;
+        if (docUri && docUri.scheme === DIFF_SCHEME) {
+          diffManager.acceptDiff(docUri);
+        }
+      },
+    ),
+    vscode.commands.registerCommand(
+      'gemini.diff.cancel',
+      (uri?: vscode.Uri) => {
+        const docUri = uri ?? vscode.window.activeTextEditor?.document.uri;
+        if (docUri && docUri.scheme === DIFF_SCHEME) {
+          diffManager.cancelDiff(docUri);
+        }
+      },
+    ),
+  );
+
+  ideServer = new IDEServer(log, diffManager);
   try {
     await ideServer.start(context);
   } catch (err) {

vybestack/llxprt-code

packages/vscode-ide-companion/src/extension.ts
@@ -7,12 +7,15 @@
 
 import * as vscode from 'vscode';
 import { IDEServer } from './ide-server';
+import { DiffContentProvider, DiffManager } from './diff-manager';
 import { createLogger } from './utils/logger';
 
 const IDE_WORKSPACE_PATH_ENV_VAR = 'LLXPRT_CODE_IDE_WORKSPACE_PATH';
+export const DIFF_SCHEME = 'llxprt-diff';
 
 let ideServer: IDEServer;
 let logger: vscode.OutputChannel;
+
 let log: (message: string) => void = () => {};
 
 function updateWorkspacePath(context: vscode.ExtensionContext) {
@@ -38,7 +41,40 @@ export async function activate(context: vscode.ExtensionContext) {
 
   updateWorkspacePath(context);
 
-  ideServer = new IDEServer(log);
+  const diffContentProvider = new DiffContentProvider();
+  const diffManager = new DiffManager(logger, diffContentProvider);
+
+  context.subscriptions.push(
+    vscode.workspace.onDidCloseTextDocument((doc) => {
+      if (doc.uri.scheme === DIFF_SCHEME) {
+        diffManager.cancelDiff(doc.uri);
+      }
+    }),
+    vscode.workspace.registerTextDocumentContentProvider(
+      DIFF_SCHEME,
+      diffContentProvider,
+    ),
+    vscode.commands.registerCommand(
+      'llxprt.diff.accept',
+      (uri?: vscode.Uri) => {
+        const docUri = uri ?? vscode.window.activeTextEditor?.document.uri;
+        if (docUri && docUri.scheme === DIFF_SCHEME) {
+          diffManager.acceptDiff(docUri);
+        }
+      },
+    ),
+    vscode.commands.registerCommand(
+      'llxprt.diff.cancel',
+      (uri?: vscode.Uri) => {
+        const docUri = uri ?? vscode.window.activeTextEditor?.document.uri;
+        if (docUri && docUri.scheme === DIFF_SCHEME) {
+          diffManager.cancelDiff(docUri);
+        }
+      },
+    ),
+  );
+
+  ideServer = new IDEServer(log, diffManager);
   try {
     await ideServer.start(context);
   } catch (err) {