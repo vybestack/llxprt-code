helldrum/gemini-cli-no-sec

feat(cli): get the run-gemini-cli version from the GitHub API (#5708)

packages/cli/src/ui/commands/setupGithubCommand.test.ts
@@ -5,13 +5,22 @@
  */
 
 import { vi, describe, expect, it, afterEach, beforeEach } from 'vitest';
-import * as child_process from 'child_process';
+import * as gitUtils from '../../utils/gitUtils.js';
 import { setupGithubCommand } from './setupGithubCommand.js';
 import { CommandContext, ToolActionReturn } from './types.js';
 
 vi.mock('child_process');
 
-describe('setupGithubCommand', () => {
+// Mock fetch globally
+global.fetch = vi.fn();
+
+vi.mock('../../utils/gitUtils.js', () => ({
+  isGitHubRepository: vi.fn(),
+  getGitRepoRoot: vi.fn(),
+  getLatestGitHubRelease: vi.fn(),
+}));
+
+describe('setupGithubCommand', async () => {
   beforeEach(() => {
     vi.resetAllMocks();
   });
@@ -20,49 +29,35 @@ describe('setupGithubCommand', () => {
     vi.restoreAllMocks();
   });
 
-  it('returns a tool action to download github workflows and handles paths', () => {
+  it('returns a tool action to download github workflows and handles paths', async () => {
     const fakeRepoRoot = '/github.com/fake/repo/root';
-    vi.mocked(child_process.execSync).mockReturnValue(fakeRepoRoot);
+    const fakeReleaseVersion = 'v1.2.3';
 
-    const result = setupGithubCommand.action?.(
+    vi.mocked(gitUtils.isGitHubRepository).mockReturnValueOnce(true);
+    vi.mocked(gitUtils.getGitRepoRoot).mockReturnValueOnce(fakeRepoRoot);
+    vi.mocked(gitUtils.getLatestGitHubRelease).mockResolvedValueOnce(
+      fakeReleaseVersion,
+    );
+
+    const result = (await setupGithubCommand.action?.(
       {} as CommandContext,
       '',
-    ) as ToolActionReturn;
-
-    expect(result.type).toBe('tool');
-    expect(result.toolName).toBe('run_shell_command');
-    expect(child_process.execSync).toHaveBeenCalledWith(
-      'git rev-parse --show-toplevel',
-      {
-        encoding: 'utf-8',
-      },
-    );
-    expect(child_process.execSync).toHaveBeenCalledWith('git remote -v', {
-      encoding: 'utf-8',
-    });
+    )) as ToolActionReturn;
 
     const { command } = result.toolArgs;
 
     const expectedSubstrings = [
+      `set -eEuo pipefail`,
       `mkdir -p "${fakeRepoRoot}/.github/workflows"`,
-      `curl -fsSL -o "${fakeRepoRoot}/.github/workflows/gemini-cli.yml"`,
-      `curl -fsSL -o "${fakeRepoRoot}/.github/workflows/gemini-issue-automated-triage.yml"`,
-      `curl -fsSL -o "${fakeRepoRoot}/.github/workflows/gemini-issue-scheduled-triage.yml"`,
-      `curl -fsSL -o "${fakeRepoRoot}/.github/workflows/gemini-pr-review.yml"`,
-      'https://raw.githubusercontent.com/google-github-actions/run-gemini-cli/refs/tags/v0/examples/workflows/',
+      `curl --fail --location --output "/github.com/fake/repo/root/.github/workflows/gemini-cli.yml" --show-error --silent`,
+      `curl --fail --location --output "/github.com/fake/repo/root/.github/workflows/gemini-issue-automated-triage.yml" --show-error --silent`,
+      `curl --fail --location --output "/github.com/fake/repo/root/.github/workflows/gemini-issue-scheduled-triage.yml" --show-error --silent`,
+      `curl --fail --location --output "/github.com/fake/repo/root/.github/workflows/gemini-pr-review.yml" --show-error --silent`,
+      `https://raw.githubusercontent.com/google-github-actions/run-gemini-cli/refs/tags/`,
     ];
 
     for (const substring of expectedSubstrings) {
       expect(command).toContain(substring);
     }
-  });
-
-  it('throws an error if git root cannot be determined', () => {
-    vi.mocked(child_process.execSync).mockReturnValue('');
-    expect(() => {
-      setupGithubCommand.action?.({} as CommandContext, '');
-    }).toThrow(
-      'Unable to determine the GitHub repository. /setup-github must be run from a git repository.',
-    );
   });
 });

vybestack/llxprt-code

packages/cli/src/ui/commands/setupGithubCommand.test.ts
@@ -5,7 +5,7 @@
  */
 
 import { vi, describe, expect, it, afterEach, beforeEach } from 'vitest';
-import * as child_process from 'child_process';
+import * as gitUtils from '../../utils/gitUtils.js';
 import { setupGithubCommand } from './setupGithubCommand.js';
 import {
   CommandContext,
@@ -15,7 +15,16 @@ import {
 
 vi.mock('child_process');
 
-describe('setupGithubCommand', () => {
+// Mock fetch globally
+global.fetch = vi.fn();
+
+vi.mock('../../utils/gitUtils.js', () => ({
+  isGitHubRepository: vi.fn(),
+  getGitRepoRoot: vi.fn(),
+  getLatestGitHubRelease: vi.fn(),
+}));
+
+describe('setupGithubCommand', async () => {
   beforeEach(() => {
     vi.resetAllMocks();
   });
@@ -38,37 +47,32 @@ describe('setupGithubCommand', () => {
   });
 
   // TODO: Re-enable these tests when the command is adapted for llxprt
-  it.skip('returns a tool action to download github workflows and handles paths', () => {
+  it.skip('returns a tool action to download github workflows and handles paths', async () => {
     const fakeRepoRoot = '/github.com/fake/repo/root';
-    vi.mocked(child_process.execSync).mockReturnValue(fakeRepoRoot);
+    const fakeReleaseVersion = 'v1.2.3';
 
-    const result = setupGithubCommand.action?.(
+    vi.mocked(gitUtils.isGitHubRepository).mockReturnValueOnce(true);
+    vi.mocked(gitUtils.getGitRepoRoot).mockReturnValueOnce(fakeRepoRoot);
+    vi.mocked(gitUtils.getLatestGitHubRelease).mockResolvedValueOnce(
+      fakeReleaseVersion,
+    );
+
+    const result = (await setupGithubCommand.action?.(
       {} as CommandContext,
       '',
-    ) as ToolActionReturn;
-
-    expect(result.type).toBe('tool');
-    expect(result.toolName).toBe('run_shell_command');
-    expect(child_process.execSync).toHaveBeenCalledWith(
-      'git rev-parse --show-toplevel',
-      {
-        encoding: 'utf-8',
-      },
-    );
-    expect(child_process.execSync).toHaveBeenCalledWith('git remote -v', {
-      encoding: 'utf-8',
-    });
+    )) as ToolActionReturn;
 
     const { command } = result.toolArgs;
 
     // TODO: Update these expectations for llxprt workflows
     const expectedSubstrings = [
+      `set -eEuo pipefail`,
       `mkdir -p "${fakeRepoRoot}/.github/workflows"`,
-      `curl -fsSL -o "${fakeRepoRoot}/.github/workflows/llxprt-cli.yml"`,
-      `curl -fsSL -o "${fakeRepoRoot}/.github/workflows/llxprt-issue-automated-triage.yml"`,
-      `curl -fsSL -o "${fakeRepoRoot}/.github/workflows/llxprt-issue-scheduled-triage.yml"`,
-      `curl -fsSL -o "${fakeRepoRoot}/.github/workflows/llxprt-pr-review.yml"`,
-      'https://raw.githubusercontent.com/vybestack/run-llxprt-code/refs/heads/main/workflows/',
+      `curl --fail --location --output "/github.com/fake/repo/root/.github/workflows/llxprt-cli.yml" --show-error --silent`,
+      `curl --fail --location --output "/github.com/fake/repo/root/.github/workflows/llxprt-issue-automated-triage.yml" --show-error --silent`,
+      `curl --fail --location --output "/github.com/fake/repo/root/.github/workflows/llxprt-issue-scheduled-triage.yml" --show-error --silent`,
+      `curl --fail --location --output "/github.com/fake/repo/root/.github/workflows/llxprt-pr-review.yml" --show-error --silent`,
+      `https://raw.githubusercontent.com/acoliver/run-llxprt-code/refs/tags/`,
     ];
 
     for (const substring of expectedSubstrings) {
@@ -77,7 +81,7 @@ describe('setupGithubCommand', () => {
   });
 
   it.skip('throws an error if git root cannot be determined', () => {
-    vi.mocked(child_process.execSync).mockReturnValue('');
+    vi.mocked(gitUtils.isGitHubRepository).mockReturnValueOnce(false);
     expect(() => {
       setupGithubCommand.action?.({} as CommandContext, '');
     }).toThrow(