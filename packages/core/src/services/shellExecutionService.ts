9cb3cd02821395e2c77aeb09102d0b79f6047803
Richie Foreman <richie.foreman@gmail.com>
2025-08-06 19:31:42 -0400

    fix(core): Improve errors in situations where the command spawn does … (#5723)

--- packages/core/src/services/shellExecutionService.ts
+++ packages/core/src/services/shellExecutionService.ts
@@ -113,7 +113,19 @@ export class ShellExecutionService {
       child.stdout.on('data', (data) => handleOutput(data, 'stdout'));
       child.stderr.on('data', (data) => handleOutput(data, 'stderr'));
       child.on('error', (err) => {
+        const { stdout, stderr, finalBuffer } = cleanup();
         error = err;
+        resolve({
+          error,
+          stdout,
+          stderr,
+          rawOutput: finalBuffer,
+          output: stdout + (stderr ? `\n${stderr}` : ''),
+          exitCode: 1,
+          signal: null,
+          aborted: false,
+          pid: child.pid,
+        });
       });
 
       const abortHandler = async () => {
@@ -134,14 +146,8 @@ export class ShellExecutionService {
 
       abortSignal.addEventListener('abort', abortHandler, { once: true });
 
-      child.on('exit', (code, signal) => {
-        exited = true;
-        abortSignal.removeEventListener('abort', abortHandler);
-
-        stdout += stripAnsi(stdoutDecoder.decode());
-        stderr += stripAnsi(stderrDecoder.decode());
-
-        const finalBuffer = Buffer.concat(outputChunks);
+      child.on('exit', (code: number, signal: NodeJS.Signals) => {
+        const { stdout, stderr, finalBuffer } = cleanup();
 
         resolve({
           rawOutput: finalBuffer,
@@ -155,6 +161,25 @@ export class ShellExecutionService {
           pid: child.pid,
         });
       });
+
+      /**
+       * Cleans up a process (and it's accompanying state) that is exiting or
+       * erroring and returns output formatted output buffers and strings
+       */
+      function cleanup() {
+        exited = true;
+        abortSignal.removeEventListener('abort', abortHandler);
+        if (stdoutDecoder) {
+          stdout += stripAnsi(stdoutDecoder.decode());
+        }
+        if (stderrDecoder) {
+          stderr += stripAnsi(stderrDecoder.decode());
+        }
+
+        const finalBuffer = Buffer.concat(outputChunks);
+
+        return { stdout, stderr, finalBuffer };
+      }
     });
 
     return { pid: child.pid, result };
