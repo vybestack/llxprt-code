972b7e686d7e559cd9893d5235655f842e5b0efb
Arya Gummadi <aryagummadi@google.com>
2025-09-01 07:24:11 -0700

    fix(build): allow builds to continue when sandbox detection fails (#7538)
    (cherry picked from commit 4fd1113905d59b3f9fcab2436c64ee170c513fa4)

--- scripts/build_sandbox.js
+++ scripts/build_sandbox.js
@@ -56,7 +56,7 @@ try {
 } catch (e) {
   console.warn('ERROR: could not detect sandbox container command');
   console.error(e);
-  process.exit(1);
+  process.exit(process.env.CI ? 1 : 0);
 }
 
 if (sandboxCommand === 'sandbox-exec') {
