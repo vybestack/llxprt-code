helldrum/gemini-cli-no-sec

feat(cli): prompt completion (#4691)
Co-authored-by: Jacob Richman <jacob314@gmail.com>

packages/cli/src/config/config.ts
@@ -542,6 +542,7 @@ export async function loadCliConfig(
     trustedFolder,
     shouldUseNodePtyShell: settings.shouldUseNodePtyShell,
     skipNextSpeakerCheck: settings.skipNextSpeakerCheck,
+    enablePromptCompletion: settings.enablePromptCompletion ?? false,
   });
 }
 

vybestack/llxprt-code

packages/cli/src/config/config.ts
@@ -818,6 +818,8 @@ export async function loadCliConfig(
     trustedFolder,
     shellReplacement: effectiveSettings.shellReplacement,
     shouldUseNodePtyShell: effectiveSettings.shouldUseNodePtyShell,
+    skipNextSpeakerCheck: effectiveSettings.skipNextSpeakerCheck,
+    enablePromptCompletion: effectiveSettings.enablePromptCompletion ?? false,
   });
 
   const enhancedConfig = config;