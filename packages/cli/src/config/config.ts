helldrum/gemini-cli-no-sec

Fix(grep): memory overflow in grep search and enhance test coverage (#5911)
Co-authored-by: Jacob Richman <jacob314@gmail.com>

packages/cli/src/config/config.ts
@@ -552,6 +552,7 @@ export async function loadCliConfig(
     folderTrust,
     interactive,
     trustedFolder,
+    useRipgrep: settings.useRipgrep,
     shouldUseNodePtyShell: settings.shouldUseNodePtyShell,
     skipNextSpeakerCheck: settings.skipNextSpeakerCheck,
     enablePromptCompletion: settings.enablePromptCompletion ?? false,

vybestack/llxprt-code

packages/cli/src/config/config.ts
@@ -831,6 +831,7 @@ export async function loadCliConfig(
     folderTrust,
     trustedFolder,
     shellReplacement: effectiveSettings.shellReplacement,
+    useRipgrep: effectiveSettings.useRipgrep,
     shouldUseNodePtyShell: effectiveSettings.shouldUseNodePtyShell,
     skipNextSpeakerCheck: effectiveSettings.skipNextSpeakerCheck,
     enablePromptCompletion: effectiveSettings.enablePromptCompletion ?? false,