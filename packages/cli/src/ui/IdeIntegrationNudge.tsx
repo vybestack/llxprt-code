helldrum/gemini-cli-no-sec

refactor(ide): Improve IDE detection discovery (#6765)

packages/cli/src/ui/IdeIntegrationNudge.tsx
@@ -88,7 +88,7 @@ export function IdeIntegrationNudge({
       <Box marginBottom={1} flexDirection="column">
         <Text>
           <Text color="yellow">{'> '}</Text>
-          {`Do you want to connect ${ideName ?? 'your'} editor to Gemini CLI?`}
+          {`Do you want to connect ${ideName ?? 'your editor'} to Gemini CLI?`}
         </Text>
         <Text dimColor>{installText}</Text>
       </Box>

vybestack/llxprt-code

packages/cli/src/ui/IdeIntegrationNudge.tsx
@@ -89,7 +89,7 @@ export function IdeIntegrationNudge({
       <Box marginBottom={1} flexDirection="column">
         <Text>
           <Text color="yellow">{'> '}</Text>
-          {`Do you want to connect ${ideName ?? 'your'} editor to LLxprt Code?`}
+          {`Do you want to connect ${ideName ?? 'your editor'} to LLxprt Code?`}
         </Text>
         <Text dimColor>{installText}</Text>
       </Box>