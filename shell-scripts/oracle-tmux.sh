c57140fe6717786698fb69f262e14b51e8b64e4d
Andrew C. Oliver <acoliver@gmail.com>
2025-10-14 20:57:50 -0300

    Remove oracle tmux helper script from repo

--- shell-scripts/oracle-tmux.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Script to connect to the Oracle instance and automatically start or attach to a tmux session named 'remote_work'
-
-echo "Connecting to Oracle Instance and managing tmux session 'remote_work'..."
-# Use the -CC flag for iTerm2 integration with terminal reset
-ssh ubuntu@170.9.234.179 -t 'stty sane; tmux -CC attach -t remote_work || tmux -CC new -s remote_work'
