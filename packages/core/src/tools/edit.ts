85c3397a87b0e214677d2a46c4a48c5d35733879
Gaurav Sehgal <sehgalgaurav@google.com>
2025-10-27 09:47:13 +0530

    add absolute file path description for windows (#12007)

--- packages/core/src/tools/edit.ts
+++ packages/core/src/tools/edit.ts
@@ -7,6 +7,7 @@
 import { promises as fsPromises } from 'fs';
 import * as path from 'path';
 import * as Diff from 'diff';
+import process from 'node:process';
 import {
   BaseDeclarativeTool,
   BaseToolInvocation,
@@ -712,7 +713,9 @@ Expectation for required parameters:
         properties: {
           file_path: {
             description:
-              "The absolute path to the file to modify. Must start with '/'.",
+              process.platform === 'win32'
+                ? "The absolute path to the file to modify (e.g., 'C:\\Users\\project\\file.txt'). Must be an absolute path."
+                : "The absolute path to the file to modify (e.g., '/home/user/project/file.txt'). Must start with '/'.",
             type: 'string',
           },
           old_string: {
