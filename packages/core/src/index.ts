a01f2fcfb0c8ed36b8d762e2577e7ec7ec6f09e7
MinWooPark-dotcom <positive1attraction@gmail.com>
2025-11-17 17:27:48 +0900

    refactor(core): export stripShellMarkers utility

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -47,6 +47,7 @@ export * from './utils/quotaErrorDetection.js';
 export * from './utils/fileUtils.js';
 export * from './utils/retry.js';
 export * from './utils/shell-utils.js';
+export * from './utils/shell-markers.js';
 export * from './utils/systemEncoding.js';
 export * from './utils/textUtils.js';
 export * from './utils/formatters.js';
