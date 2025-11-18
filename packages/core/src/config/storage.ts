helldrum/gemini-cli-no-sec

Feat(security) - Create a class for Oauth credential (#7745)
Co-authored-by: Shi Shu <shii@google.com>

packages/core/src/config/storage.ts
@@ -11,6 +11,7 @@ import * as fs from 'node:fs';
 
 export const GEMINI_DIR = '.gemini';
 export const GOOGLE_ACCOUNTS_FILENAME = 'google_accounts.json';
+export const OAUTH_FILE = 'oauth_creds.json';
 const TMP_DIR_NAME = 'tmp';
 
 export class Storage {
@@ -71,7 +72,7 @@ export class Storage {
   }
 
   static getOAuthCredsPath(): string {
-    return path.join(Storage.getGlobalGeminiDir(), 'oauth_creds.json');
+    return path.join(Storage.getGlobalGeminiDir(), OAUTH_FILE);
   }
 
   getProjectRoot(): string {

vybestack/llxprt-code

(cherry picked from commit af52b04e6f75df093df5daa39532e873912a27d5)

packages/core/src/config/storage.ts
@@ -11,6 +11,7 @@ import * as fs from 'fs';
 
 export const LLXPRT_DIR = '.llxprt';
 export const PROVIDER_ACCOUNTS_FILENAME = 'provider_accounts.json';
+export const OAUTH_FILE = 'oauth_creds.json';
 const TMP_DIR_NAME = 'tmp';
 
 export class Storage {
@@ -75,7 +76,7 @@ export class Storage {
   }
 
   static getOAuthCredsPath(): string {
-    return path.join(Storage.getGlobalLlxprtDir(), 'oauth_creds.json');
+    return path.join(Storage.getGlobalLlxprtDir(), OAUTH_FILE);
   }
 
   getProjectRoot(): string {