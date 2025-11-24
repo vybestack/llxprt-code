Bare-Corporation/bare-ai-cli

Adding list sub command to memoryCommand to list the path of GEMINI.md files (#10108)
Co-authored-by: Abhi <43648792+abhipatel12@users.noreply.github.com>

packages/cli/src/ui/commands/memoryCommand.ts
@@ -83,7 +83,7 @@ export const memoryCommand: SlashCommand = {
         try {
           const config = await context.services.config;
           if (config) {
-            const { memoryContent, fileCount } =
+            const { memoryContent, fileCount, filePaths } =
               await loadServerHierarchicalMemory(
                 config.getWorkingDir(),
                 config.shouldLoadMemoryFromIncludeDirectories()
@@ -100,6 +100,7 @@ export const memoryCommand: SlashCommand = {
               );
             config.setUserMemory(memoryContent);
             config.setGeminiMdFileCount(fileCount);
+            config.setGeminiMdFilePaths(filePaths);
 
             const successMessage =
               memoryContent.length > 0
@@ -124,6 +125,28 @@ export const memoryCommand: SlashCommand = {
             Date.now(),
           );
         }
+      },
+    },
+    {
+      name: 'list',
+      description: 'Lists the paths of the GEMINI.md files in use.',
+      kind: CommandKind.BUILT_IN,
+      action: async (context) => {
+        const filePaths = context.services.config?.getGeminiMdFilePaths() || [];
+        const fileCount = filePaths.length;
+
+        const messageContent =
+          fileCount > 0
+            ? `There are ${fileCount} GEMINI.md file(s) in use:\n\n${filePaths.join('\n')}`
+            : 'No GEMINI.md files in use.';
+
+        context.ui.addItem(
+          {
+            type: MessageType.INFO,
+            text: messageContent,
+          },
+          Date.now(),
+        );
       },
     },
   ],

vybestack/llxprt-code

packages/cli/src/ui/commands/memoryCommand.ts
@@ -86,7 +86,7 @@ export const memoryCommand: SlashCommand = {
         try {
           const config = await context.services.config;
           if (config) {
-            const { memoryContent, fileCount } =
+            const { memoryContent, fileCount, filePaths } =
               await loadServerHierarchicalMemory(
                 config.getWorkingDir(),
                 config.shouldLoadMemoryFromIncludeDirectories()
@@ -102,6 +102,7 @@ export const memoryCommand: SlashCommand = {
               );
             config.setUserMemory(memoryContent);
             config.setLlxprtMdFileCount(fileCount);
+            config.setLlxprtMdFilePaths(filePaths);
 
             const successMessage =
               memoryContent.length > 0
@@ -126,6 +127,28 @@ export const memoryCommand: SlashCommand = {
             Date.now(),
           );
         }
+      },
+    },
+    {
+      name: 'list',
+      description: 'Lists the paths of the LLXPRT.md files in use.',
+      kind: CommandKind.BUILT_IN,
+      action: async (context) => {
+        const filePaths = context.services.config?.getLlxprtMdFilePaths() || [];
+        const fileCount = filePaths.length;
+
+        const messageContent =
+          fileCount > 0
+            ? `There are ${fileCount} LLXPRT.md file(s) in use:\n\n${filePaths.join('\n')}`
+            : 'No LLXPRT.md files in use.';
+
+        context.ui.addItem(
+          {
+            type: MessageType.INFO,
+            text: messageContent,
+          },
+          Date.now(),
+        );
       },
     },
   ],