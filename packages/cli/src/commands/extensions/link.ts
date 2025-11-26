0e1317f9a8b25bbadacfe2e3aec1dfe8f1c67a8b
kevinjwang1 <kevinjwang@google.com>
2025-11-26 19:22:26 +0000

    Add consent flag to Link command (#13832)

--- packages/cli/src/commands/extensions/link.ts
+++ packages/cli/src/commands/extensions/link.ts
@@ -17,6 +17,7 @@ import { exitCli } from '../utils.js';
 
 interface InstallArgs {
   path: string;
+  consent?: boolean;
 }
 
 export async function handleLink(args: InstallArgs) {
@@ -25,10 +26,13 @@ export async function handleLink(args: InstallArgs) {
       source: args.path,
       type: 'link',
     };
+    const requestConsent = args.consent
+      ? () => Promise.resolve(true)
+      : requestConsentNonInteractive;
     const workspaceDir = process.cwd();
     const extensionName = await installOrUpdateExtension(
       installMetadata,
-      requestConsentNonInteractive,
+      requestConsent,
       workspaceDir,
     );
     const extension = loadExtensionByName(extensionName, workspaceDir);
@@ -51,10 +55,17 @@ export const linkCommand: CommandModule = {
         describe: 'The name of the extension to link.',
         type: 'string',
       })
+      .option('consent', {
+        describe:
+          'Acknowledge the security risks of installing an extension and skip the confirmation prompt.',
+        type: 'boolean',
+        default: false,
+      })
       .check((_) => true),
   handler: async (argv) => {
     await handleLink({
       path: argv['path'] as string,
+      consent: argv['consent'] as boolean | undefined,
     });
     await exitCli();
   },
