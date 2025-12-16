Bare-Corporation/bare-ai-cli

fix(cli) : fixed bug #8310 where /memory refresh will create discrepancies with initial memory load ignoring settings/config for trusted folder and file filters (#10611)
Co-authored-by: Bryan Morgan <bryanmorgan@google.com>

packages/cli/src/ui/commands/memoryCommand.test.ts
@@ -13,10 +13,10 @@ import { MessageType } from '../types.js';
 import type { LoadedSettings } from '../../config/settings.js';
 import {
   getErrorMessage,
-  loadServerHierarchicalMemory,
   type FileDiscoveryService,
 } from '@google/gemini-cli-core';
 import type { LoadServerHierarchicalMemoryResponse } from '@google/gemini-cli-core/index.js';
+import { loadHierarchicalGeminiMemory } from '../../config/config.js';
 
 vi.mock('@google/gemini-cli-core', async (importOriginal) => {
   const original =
@@ -27,11 +27,19 @@ vi.mock('@google/gemini-cli-core', async (importOriginal) => {
       if (error instanceof Error) return error.message;
       return String(error);
     }),
-    loadServerHierarchicalMemory: vi.fn(),
   };
 });
 
-const mockLoadServerHierarchicalMemory = loadServerHierarchicalMemory as Mock;
+vi.mock('../../config/config.js', async (importOriginal) => {
+  const original =
+    await importOriginal<typeof import('../../config/config.js')>();
+  return {
+    ...original,
+    loadHierarchicalGeminiMemory: vi.fn(),
+  };
+});
+
+const mockLoadHierarchicalGeminiMemory = loadHierarchicalGeminiMemory as Mock;
 
 describe('memoryCommand', () => {
   let mockContext: CommandContext;
@@ -177,7 +185,7 @@ describe('memoryCommand', () => {
           ignore: [],
           include: [],
         }),
-        getFolderTrust: () => false,
+        isTrustedFolder: () => false,
       };
 
       mockContext = createMockCommandContext({
@@ -186,11 +194,17 @@ describe('memoryCommand', () => {
           settings: {
             merged: {
               memoryDiscoveryMaxDirs: 1000,
+              context: {
+                importFormat: 'tree',
+              },
             },
           } as LoadedSettings,
         },
+        ui: {
+          setGeminiMdFileCount: vi.fn(),
+        },
       });
-      mockLoadServerHierarchicalMemory.mockClear();
+      mockLoadHierarchicalGeminiMemory.mockClear();
     });
 
     it('should display success message when memory is refreshed with content', async () => {
@@ -201,7 +215,7 @@ describe('memoryCommand', () => {
         fileCount: 2,
         filePaths: ['/path/one/GEMINI.md', '/path/two/GEMINI.md'],
       };
-      mockLoadServerHierarchicalMemory.mockResolvedValue(refreshResult);
+      mockLoadHierarchicalGeminiMemory.mockResolvedValue(refreshResult);
 
       await refreshCommand.action(mockContext, '');
 
@@ -213,7 +227,7 @@ describe('memoryCommand', () => {
         expect.any(Number),
       );
 
-      expect(loadServerHierarchicalMemory).toHaveBeenCalledOnce();
+      expect(mockLoadHierarchicalGeminiMemory).toHaveBeenCalledOnce();
       expect(mockSetUserMemory).toHaveBeenCalledWith(
         refreshResult.memoryContent,
       );
@@ -222,6 +236,9 @@ describe('memoryCommand', () => {
       );
       expect(mockSetGeminiMdFilePaths).toHaveBeenCalledWith(
         refreshResult.filePaths,
+      );
+      expect(mockContext.ui.setGeminiMdFileCount).toHaveBeenCalledWith(
+        refreshResult.fileCount,
       );
 
       expect(mockContext.ui.addItem).toHaveBeenCalledWith(
@@ -237,11 +254,11 @@ describe('memoryCommand', () => {
       if (!refreshCommand.action) throw new Error('Command has no action');
 
       const refreshResult = { memoryContent: '', fileCount: 0, filePaths: [] };
-      mockLoadServerHierarchicalMemory.mockResolvedValue(refreshResult);
+      mockLoadHierarchicalGeminiMemory.mockResolvedValue(refreshResult);
 
       await refreshCommand.action(mockContext, '');
 
-      expect(loadServerHierarchicalMemory).toHaveBeenCalledOnce();
+      expect(mockLoadHierarchicalGeminiMemory).toHaveBeenCalledOnce();
       expect(mockSetUserMemory).toHaveBeenCalledWith('');
       expect(mockSetGeminiMdFileCount).toHaveBeenCalledWith(0);
       expect(mockSetGeminiMdFilePaths).toHaveBeenCalledWith([]);
@@ -259,11 +276,11 @@ describe('memoryCommand', () => {
       if (!refreshCommand.action) throw new Error('Command has no action');
 
       const error = new Error('Failed to read memory files.');
-      mockLoadServerHierarchicalMemory.mockRejectedValue(error);
+      mockLoadHierarchicalGeminiMemory.mockRejectedValue(error);
 
       await refreshCommand.action(mockContext, '');
 
-      expect(loadServerHierarchicalMemory).toHaveBeenCalledOnce();
+      expect(mockLoadHierarchicalGeminiMemory).toHaveBeenCalledOnce();
       expect(mockSetUserMemory).not.toHaveBeenCalled();
       expect(mockSetGeminiMdFileCount).not.toHaveBeenCalled();
       expect(mockSetGeminiMdFilePaths).not.toHaveBeenCalled();
@@ -298,7 +315,7 @@ describe('memoryCommand', () => {
         expect.any(Number),
       );
 
-      expect(loadServerHierarchicalMemory).not.toHaveBeenCalled();
+      expect(mockLoadHierarchicalGeminiMemory).not.toHaveBeenCalled();
     });
   });
 

packages/cli/src/ui/commands/memoryCommand.ts
@@ -4,11 +4,9 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import {
-  getErrorMessage,
-  loadServerHierarchicalMemory,
-} from '@google/gemini-cli-core';
+import { getErrorMessage } from '@google/gemini-cli-core';
 import { MessageType } from '../types.js';
+import { loadHierarchicalGeminiMemory } from '../../config/config.js';
 import type { SlashCommand, SlashCommandActionReturn } from './types.js';
 import { CommandKind } from './types.js';
 
@@ -82,25 +80,26 @@ export const memoryCommand: SlashCommand = {
 
         try {
           const config = await context.services.config;
+          const settings = context.services.settings;
           if (config) {
             const { memoryContent, fileCount, filePaths } =
-              await loadServerHierarchicalMemory(
+              await loadHierarchicalGeminiMemory(
                 config.getWorkingDir(),
                 config.shouldLoadMemoryFromIncludeDirectories()
                   ? config.getWorkspaceContext().getDirectories()
                   : [],
                 config.getDebugMode(),
                 config.getFileService(),
+                settings.merged,
                 config.getExtensionContextFilePaths(),
-                config.getFolderTrust(),
-                context.services.settings.merged.context?.importFormat ||
-                  'tree', // Use setting or default to 'tree'
+                config.isTrustedFolder(),
+                settings.merged.context?.importFormat || 'tree',
                 config.getFileFilteringOptions(),
-                context.services.settings.merged.context?.discoveryMaxDirs,
               );
             config.setUserMemory(memoryContent);
             config.setGeminiMdFileCount(fileCount);
             config.setGeminiMdFilePaths(filePaths);
+            context.ui.setGeminiMdFileCount(fileCount);
 
             const successMessage =
               memoryContent.length > 0

packages/core/src/config/config.ts
@@ -144,14 +144,14 @@ export interface ExtensionInstallMetadata {
 
 import type { FileFilteringOptions } from './constants.js';
 import {
-  DEFAULT_MEMORY_FILE_FILTERING_OPTIONS,
   DEFAULT_FILE_FILTERING_OPTIONS,
+  DEFAULT_MEMORY_FILE_FILTERING_OPTIONS,
 } from './constants.js';
 
 export type { FileFilteringOptions };
 export {
-  DEFAULT_MEMORY_FILE_FILTERING_OPTIONS,
   DEFAULT_FILE_FILTERING_OPTIONS,
+  DEFAULT_MEMORY_FILE_FILTERING_OPTIONS,
 };
 
 export const DEFAULT_TRUNCATE_TOOL_OUTPUT_THRESHOLD = 4_000_000;
@@ -405,8 +405,12 @@ export class Config {
     this.usageStatisticsEnabled = params.usageStatisticsEnabled ?? true;
 
     this.fileFiltering = {
-      respectGitIgnore: params.fileFiltering?.respectGitIgnore ?? true,
-      respectGeminiIgnore: params.fileFiltering?.respectGeminiIgnore ?? true,
+      respectGitIgnore:
+        params.fileFiltering?.respectGitIgnore ??
+        DEFAULT_FILE_FILTERING_OPTIONS.respectGitIgnore,
+      respectGeminiIgnore:
+        params.fileFiltering?.respectGeminiIgnore ??
+        DEFAULT_FILE_FILTERING_OPTIONS.respectGeminiIgnore,
       enableRecursiveFileSearch:
         params.fileFiltering?.enableRecursiveFileSearch ?? true,
       disableFuzzySearch: params.fileFiltering?.disableFuzzySearch ?? false,
@@ -885,6 +889,9 @@ export class Config {
     return this.ideMode;
   }
 
+  /***
+   * TODO: Review if this is actually used at it seems it is only used but the FileCommandLoader
+   */
   getFolderTrustFeature(): boolean {
     return this.folderTrustFeature;
   }

vybestack/llxprt-code

packages/cli/src/ui/commands/memoryCommand.test.ts
@@ -13,10 +13,10 @@ import { MessageType } from '../types.js';
 import { LoadedSettings } from '../../config/settings.js';
 import {
   getErrorMessage,
-  loadServerHierarchicalMemory,
   type FileDiscoveryService,
   type LoadServerHierarchicalMemoryResponse,
 } from '@vybestack/llxprt-code-core';
+import { loadHierarchicalLlxprtMemory } from '../../config/config.js';
 
 vi.mock('@vybestack/llxprt-code-core', async (importOriginal) => {
   const original =
@@ -27,11 +27,19 @@ vi.mock('@vybestack/llxprt-code-core', async (importOriginal) => {
       if (error instanceof Error) return error.message;
       return String(error);
     }),
-    loadServerHierarchicalMemory: vi.fn(),
   };
 });
 
-const mockLoadServerHierarchicalMemory = loadServerHierarchicalMemory as Mock;
+vi.mock('../../config/config.js', async (importOriginal) => {
+  const original =
+    await importOriginal<typeof import('../../config/config.js')>();
+  return {
+    ...original,
+    loadHierarchicalLlxprtMemory: vi.fn(),
+  };
+});
+
+const mockLoadHierarchicalLlxprtMemory = loadHierarchicalLlxprtMemory as Mock;
 
 describe('memoryCommand', () => {
   let mockContext: CommandContext;
@@ -308,7 +316,7 @@ describe('memoryCommand', () => {
           ignore: [],
           include: [],
         }),
-        getFolderTrust: () => false,
+        isTrustedFolder: () => false,
       };
 
       mockContext = createMockCommandContext({
@@ -317,11 +325,17 @@ describe('memoryCommand', () => {
           settings: {
             merged: {
               memoryDiscoveryMaxDirs: 1000,
+              context: {
+                importFormat: 'tree',
+              },
             },
           } as LoadedSettings,
         },
+        ui: {
+          setGeminiMdFileCount: vi.fn(),
+        },
       });
-      mockLoadServerHierarchicalMemory.mockClear();
+      mockLoadHierarchicalLlxprtMemory.mockClear();
     });
 
     it('should display success message when memory is refreshed with content', async () => {
@@ -332,7 +346,7 @@ describe('memoryCommand', () => {
         fileCount: 2,
         filePaths: ['/path/one/GEMINI.md', '/path/two/GEMINI.md'],
       };
-      mockLoadServerHierarchicalMemory.mockResolvedValue(refreshResult);
+      mockLoadHierarchicalLlxprtMemory.mockResolvedValue(refreshResult);
 
       await refreshCommand.action(mockContext, '');
 
@@ -344,7 +358,7 @@ describe('memoryCommand', () => {
         expect.any(Number),
       );
 
-      expect(loadServerHierarchicalMemory).toHaveBeenCalledOnce();
+      expect(mockLoadHierarchicalLlxprtMemory).toHaveBeenCalledOnce();
       expect(mockSetUserMemory).toHaveBeenCalledWith(
         refreshResult.memoryContent,
       );
@@ -353,6 +367,9 @@ describe('memoryCommand', () => {
       );
       expect(mockSetLlxprtMdFilePaths).toHaveBeenCalledWith(
         refreshResult.filePaths,
+      );
+      expect(mockContext.ui.setGeminiMdFileCount).toHaveBeenCalledWith(
+        refreshResult.fileCount,
       );
 
       expect(mockContext.ui.addItem).toHaveBeenCalledWith(
@@ -368,11 +385,11 @@ describe('memoryCommand', () => {
       if (!refreshCommand.action) throw new Error('Command has no action');
 
       const refreshResult = { memoryContent: '', fileCount: 0, filePaths: [] };
-      mockLoadServerHierarchicalMemory.mockResolvedValue(refreshResult);
+      mockLoadHierarchicalLlxprtMemory.mockResolvedValue(refreshResult);
 
       await refreshCommand.action(mockContext, '');
 
-      expect(loadServerHierarchicalMemory).toHaveBeenCalledOnce();
+      expect(mockLoadHierarchicalLlxprtMemory).toHaveBeenCalledOnce();
       expect(mockSetUserMemory).toHaveBeenCalledWith('');
       expect(mockSetLlxprtMdFileCount).toHaveBeenCalledWith(0);
       expect(mockSetLlxprtMdFilePaths).toHaveBeenCalledWith([]);
@@ -390,11 +407,11 @@ describe('memoryCommand', () => {
       if (!refreshCommand.action) throw new Error('Command has no action');
 
       const error = new Error('Failed to read memory files.');
-      mockLoadServerHierarchicalMemory.mockRejectedValue(error);
+      mockLoadHierarchicalLlxprtMemory.mockRejectedValue(error);
 
       await refreshCommand.action(mockContext, '');
 
-      expect(loadServerHierarchicalMemory).toHaveBeenCalledOnce();
+      expect(mockLoadHierarchicalLlxprtMemory).toHaveBeenCalledOnce();
       expect(mockSetUserMemory).not.toHaveBeenCalled();
       expect(mockSetLlxprtMdFileCount).not.toHaveBeenCalled();
       expect(mockSetLlxprtMdFilePaths).not.toHaveBeenCalled();
@@ -429,7 +446,7 @@ describe('memoryCommand', () => {
         expect.any(Number),
       );
 
-      expect(loadServerHierarchicalMemory).not.toHaveBeenCalled();
+      expect(mockLoadHierarchicalLlxprtMemory).not.toHaveBeenCalled();
     });
   });
 

packages/cli/src/ui/commands/memoryCommand.ts
@@ -4,16 +4,11 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import {
-  getErrorMessage,
-  loadServerHierarchicalMemory,
-} from '@vybestack/llxprt-code-core';
+import { getErrorMessage } from '@vybestack/llxprt-code-core';
 import { MessageType } from '../types.js';
-import {
-  CommandKind,
-  SlashCommand,
-  SlashCommandActionReturn,
-} from './types.js';
+import { loadHierarchicalLlxprtMemory } from '../../config/config.js';
+import type { SlashCommand, SlashCommandActionReturn } from './types.js';
+import { CommandKind } from './types.js';
 
 export const memoryCommand: SlashCommand = {
   name: 'memory',
@@ -148,17 +143,19 @@ export const memoryCommand: SlashCommand = {
 
         try {
           const config = await context.services.config;
+          const settings = context.services.settings;
           if (config) {
             const { memoryContent, fileCount, filePaths } =
-              await loadServerHierarchicalMemory(
+              await loadHierarchicalLlxprtMemory(
                 config.getWorkingDir(),
                 config.shouldLoadMemoryFromIncludeDirectories()
                   ? config.getWorkspaceContext().getDirectories()
                   : [],
                 config.getDebugMode(),
                 config.getFileService(),
+                settings.merged,
                 config.getExtensionContextFilePaths(),
-                config.getFolderTrust(),
+                config.isTrustedFolder(),
                 context.services.settings.merged.ui?.memoryImportFormat ||
                   'tree', // Use setting or default to 'tree'
                 config.getFileFilteringOptions(),
@@ -167,6 +164,7 @@ export const memoryCommand: SlashCommand = {
             config.setUserMemory(memoryContent);
             config.setLlxprtMdFileCount(fileCount);
             config.setLlxprtMdFilePaths(filePaths);
+            context.ui.setGeminiMdFileCount(fileCount);
 
             const successMessage =
               memoryContent.length > 0

packages/core/src/config/config.ts
@@ -197,14 +197,14 @@ export interface ExtensionInstallMetadata {
 
 import type { FileFilteringOptions } from './constants.js';
 import {
-  DEFAULT_MEMORY_FILE_FILTERING_OPTIONS,
   DEFAULT_FILE_FILTERING_OPTIONS,
+  DEFAULT_MEMORY_FILE_FILTERING_OPTIONS,
 } from './constants.js';
 
 export type { FileFilteringOptions };
 export {
-  DEFAULT_MEMORY_FILE_FILTERING_OPTIONS,
   DEFAULT_FILE_FILTERING_OPTIONS,
+  DEFAULT_MEMORY_FILE_FILTERING_OPTIONS,
 };
 
 export const DEFAULT_TRUNCATE_TOOL_OUTPUT_THRESHOLD = 4_000_000;
@@ -600,8 +600,12 @@ export class Config {
     this.usageStatisticsEnabled = params.usageStatisticsEnabled ?? true;
 
     this.fileFiltering = {
-      respectGitIgnore: params.fileFiltering?.respectGitIgnore ?? true,
-      respectLlxprtIgnore: params.fileFiltering?.respectLlxprtIgnore ?? true,
+      respectGitIgnore:
+        params.fileFiltering?.respectGitIgnore ??
+        DEFAULT_FILE_FILTERING_OPTIONS.respectGitIgnore,
+      respectLlxprtIgnore:
+        params.fileFiltering?.respectLlxprtIgnore ??
+        DEFAULT_FILE_FILTERING_OPTIONS.respectLlxprtIgnore,
       enableRecursiveFileSearch:
         params.fileFiltering?.enableRecursiveFileSearch ?? true,
       disableFuzzySearch: params.fileFiltering?.disableFuzzySearch ?? false,
@@ -1307,6 +1311,9 @@ export class Config {
     return this.ideMode;
   }
 
+  /***
+   * TODO: Review if this is actually used at it seems it is only used but the FileCommandLoader
+   */
   getFolderTrustFeature(): boolean {
     return this.folderTrustFeature;
   }