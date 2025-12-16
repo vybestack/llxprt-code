Bare-Corporation/bare-ai-cli

fix(core): Use shell for spawn on Windows (#9995)

packages/core/src/ide/ide-installer.test.ts
@@ -119,7 +119,24 @@ describe('ide-installer', () => {
             'google.gemini-cli-vscode-ide-companion',
             '--force',
           ],
-          { stdio: 'pipe' },
+          { stdio: 'pipe', shell: false },
+        );
+      });
+
+      it('installs the extension using code cli on windows', async () => {
+        const { installer } = setup({
+          platform: 'win32',
+          execSync: () => 'C:\\Program Files\\Microsoft VS Code\\bin\\code.cmd',
+        });
+        await installer.install();
+        expect(child_process.spawnSync).toHaveBeenCalledWith(
+          'C:\\Program Files\\Microsoft VS Code\\bin\\code.cmd',
+          [
+            '--install-extension',
+            'google.gemini-cli-vscode-ide-companion',
+            '--force',
+          ],
+          { stdio: 'pipe', shell: true },
         );
       });
 

vybestack/llxprt-code

packages/core/src/ide/ide-installer.test.ts
@@ -12,6 +12,7 @@ vi.mock('node:child_process', async (importOriginal) => {
   return {
     ...actual,
     execSync: vi.fn(),
+    spawnSync: vi.fn(() => ({ status: 0 })),
   };
 });
 vi.mock('fs');
@@ -111,9 +112,31 @@ describe('ide-installer', () => {
           platform: 'linux',
         });
         await installer.install();
-        expect(child_process.execSync).toHaveBeenCalledWith(
-          '"code" --install-extension vybestack.llxprt-code-vscode-ide-companion --force',
-          { stdio: 'pipe' },
+        expect(child_process.spawnSync).toHaveBeenCalledWith(
+          'code',
+          [
+            '--install-extension',
+            'vybestack.llxprt-code-vscode-ide-companion',
+            '--force',
+          ],
+          { stdio: 'pipe', shell: false },
+        );
+      });
+
+      it('installs the extension using code cli on windows', async () => {
+        const { installer } = setup({
+          platform: 'win32',
+          execSync: () => 'C:\\Program Files\\Microsoft VS Code\\bin\\code.cmd',
+        });
+        await installer.install();
+        expect(child_process.spawnSync).toHaveBeenCalledWith(
+          'C:\\Program Files\\Microsoft VS Code\\bin\\code.cmd',
+          [
+            '--install-extension',
+            'vybestack.llxprt-code-vscode-ide-companion',
+            '--force',
+          ],
+          { stdio: 'pipe', shell: true },
         );
       });
 