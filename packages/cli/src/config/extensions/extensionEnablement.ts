Bare-Corporation/bare-ai-cli

use coreEvents.emitFeedback in extension enablement (#11985)

packages/cli/src/config/extensions/extensionEnablement.ts
@@ -6,7 +6,7 @@
 
 import fs from 'node:fs';
 import path from 'node:path';
-import type { GeminiCLIExtension } from '@google/gemini-cli-core';
+import { coreEvents, type GeminiCLIExtension } from '@google/gemini-cli-core';
 import { ExtensionStorage } from './storage.js';
 
 export interface ExtensionEnablementConfig {
@@ -129,7 +129,7 @@ export class ExtensionEnablementManager {
       if (
         !extensions.some((ext) => ext.name.toLowerCase() === name.toLowerCase())
       ) {
-        console.error(`Extension not found: ${name}`);
+        coreEvents.emitFeedback('error', `Extension not found: ${name}`);
       }
     }
   }
@@ -188,7 +188,11 @@ export class ExtensionEnablementManager {
       ) {
         return {};
       }
-      console.error('Error reading extension enablement config:', error);
+      coreEvents.emitFeedback(
+        'error',
+        'Failed to read extension enablement config.',
+        error,
+      );
       return {};
     }
   }

vybestack/llxprt-code

packages/cli/src/config/extensions/extensionEnablement.ts
@@ -6,7 +6,7 @@
 
 import fs from 'node:fs';
 import path from 'node:path';
-import type { GeminiCLIExtension } from '@vybestack/llxprt-code-core';
+import { coreEvents, type GeminiCLIExtension } from '@vybestack/llxprt-code-core';
 
 export interface ExtensionEnablementConfig {
   overrides: string[];
@@ -125,7 +125,7 @@ export class ExtensionEnablementManager {
       if (
         !extensions.some((ext) => ext.name.toLowerCase() === name.toLowerCase())
       ) {
-        console.error(`Extension not found: ${name}`);
+        coreEvents.emitFeedback('error', `Extension not found: ${name}`);
       }
     }
   }
@@ -184,7 +184,11 @@ export class ExtensionEnablementManager {
       ) {
         return {};
       }
-      console.error('Error reading extension enablement config:', error);
+      coreEvents.emitFeedback(
+        'error',
+        'Failed to read extension enablement config.',
+        error,
+      );
       return {};
     }
   }