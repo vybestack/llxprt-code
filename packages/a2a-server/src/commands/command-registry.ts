Bare-Corporation/bare-ai-cli

Adds executeCommand endpoint with support for /extensions list (#11515)

packages/a2a-server/src/commands/command-registry.ts
@@ -1,0 +1,33 @@
+/**
+ * @license
+ * Copyright 2025 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { ListExtensionsCommand } from './list-extensions.js';
+import type { Config } from '@google/gemini-cli-core';
+
+export interface Command {
+  readonly names: string[];
+  execute(config: Config, args: string[]): Promise<unknown>;
+}
+
+class CommandRegistry {
+  private readonly commands = new Map<string, Command>();
+
+  constructor() {
+    this.register(new ListExtensionsCommand());
+  }
+
+  register(command: Command) {
+    for (const name of command.names) {
+      this.commands.set(name, command);
+    }
+  }
+
+  get(commandName: string): Command | undefined {
+    return this.commands.get(commandName);
+  }
+}
+
+export const commandRegistry = new CommandRegistry();

packages/a2a-server/src/commands/list-extensions.test.ts
@@ -1,0 +1,39 @@
+/**
+ * @license
+ * Copyright 2025 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { describe, it, expect, vi } from 'vitest';
+import { ListExtensionsCommand } from './list-extensions.js';
+import type { Config } from '@google/gemini-cli-core';
+
+const mockListExtensions = vi.hoisted(() => vi.fn());
+vi.mock('@google/gemini-cli-core', async (importOriginal) => {
+  const original =
+    await importOriginal<typeof import('@google/gemini-cli-core')>();
+
+  return {
+    ...original,
+    listExtensions: mockListExtensions,
+  };
+});
+
+describe('ListExtensionsCommand', () => {
+  it('should have the correct names', () => {
+    const command = new ListExtensionsCommand();
+    expect(command.names).toEqual(['extensions', 'extensions list']);
+  });
+
+  it('should call listExtensions with the provided config', async () => {
+    const command = new ListExtensionsCommand();
+    const mockConfig = {} as Config;
+    const mockExtensions = [{ name: 'ext1' }];
+    mockListExtensions.mockReturnValue(mockExtensions);
+
+    const result = await command.execute(mockConfig, []);
+
+    expect(result).toEqual(mockExtensions);
+    expect(mockListExtensions).toHaveBeenCalledWith(mockConfig);
+  });
+});

packages/a2a-server/src/commands/list-extensions.ts
@@ -1,0 +1,16 @@
+/**
+ * @license
+ * Copyright 2025 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { listExtensions, type Config } from '@google/gemini-cli-core';
+import type { Command } from './command-registry.js';
+
+export class ListExtensionsCommand implements Command {
+  readonly names = ['extensions', 'extensions list'];
+
+  async execute(config: Config, _: string[]): Promise<unknown> {
+    return listExtensions(config);
+  }
+}

packages/cli/src/ui/commands/extensionsCommand.ts
@@ -4,9 +4,10 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+import { listExtensions } from '@google/gemini-cli-core';
 import type { ExtensionUpdateInfo } from '../../config/extension.js';
 import { getErrorMessage } from '../../utils/errors.js';
-import { MessageType } from '../types.js';
+import { MessageType, type HistoryItemExtensionsList } from '../types.js';
 import {
   type CommandContext,
   type SlashCommand,
@@ -14,12 +15,14 @@ import {
 } from './types.js';
 
 async function listAction(context: CommandContext) {
-  context.ui.addItem(
-    {
-      type: MessageType.EXTENSIONS_LIST,
-    },
-    Date.now(),
-  );
+  const historyItem: HistoryItemExtensionsList = {
+    type: MessageType.EXTENSIONS_LIST,
+    extensions: context.services.config
+      ? listExtensions(context.services.config)
+      : [],
+  };
+
+  context.ui.addItem(historyItem, Date.now());
 }
 
 function updateAction(context: CommandContext, args: string): Promise<void> {
@@ -42,6 +45,14 @@ function updateAction(context: CommandContext, args: string): Promise<void> {
   const updateComplete = new Promise<ExtensionUpdateInfo[]>(
     (resolve) => (resolveUpdateComplete = resolve),
   );
+
+  const historyItem: HistoryItemExtensionsList = {
+    type: MessageType.EXTENSIONS_LIST,
+    extensions: context.services.config
+      ? listExtensions(context.services.config)
+      : [],
+  };
+
   updateComplete.then((updateInfos) => {
     if (updateInfos.length === 0) {
       context.ui.addItem(
@@ -52,19 +63,13 @@ function updateAction(context: CommandContext, args: string): Promise<void> {
         Date.now(),
       );
     }
-    context.ui.addItem(
-      {
-        type: MessageType.EXTENSIONS_LIST,
-      },
-      Date.now(),
-    );
+
+    context.ui.addItem(historyItem, Date.now());
     context.ui.setPendingItem(null);
   });
 
   try {
-    context.ui.setPendingItem({
-      type: MessageType.EXTENSIONS_LIST,
-    });
+    context.ui.setPendingItem(historyItem);
 
     context.ui.dispatchExtensionStateUpdate({
       type: 'SCHEDULE_UPDATE',
@@ -77,7 +82,7 @@ function updateAction(context: CommandContext, args: string): Promise<void> {
       },
     });
     if (names?.length) {
-      const extensions = context.services.config!.getExtensions();
+      const extensions = listExtensions(context.services.config!);
       for (const name of names) {
         const extension = extensions.find(
           (extension) => extension.name === name,
@@ -120,7 +125,9 @@ const updateExtensionsCommand: SlashCommand = {
   kind: CommandKind.BUILT_IN,
   action: updateAction,
   completion: async (context, partialArg) => {
-    const extensions = context.services.config?.getExtensions() ?? [];
+    const extensions = context.services.config
+      ? listExtensions(context.services.config)
+      : [];
     const extensionNames = extensions.map((ext) => ext.name);
     const suggestions = extensionNames.filter((name) =>
       name.startsWith(partialArg),

packages/cli/src/ui/components/views/ExtensionsList.tsx
@@ -4,15 +4,20 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+import type React from 'react';
 import { Box, Text } from 'ink';
 import { useUIState } from '../../contexts/UIStateContext.js';
 import { ExtensionUpdateState } from '../../state/extensions.js';
+import type { GeminiCLIExtension } from '@google/gemini-cli-core';
 
-export const ExtensionsList = () => {
-  const { commandContext, extensionsUpdateState } = useUIState();
-  const allExtensions = commandContext.services.config!.getExtensions();
+interface ExtensionsList {
+  extensions: readonly GeminiCLIExtension[];
+}
 
-  if (allExtensions.length === 0) {
+export const ExtensionsList: React.FC<ExtensionsList> = ({ extensions }) => {
+  const { extensionsUpdateState } = useUIState();
+
+  if (extensions.length === 0) {
     return <Text>No extensions installed.</Text>;
   }
 
@@ -20,7 +25,7 @@ export const ExtensionsList = () => {
     <Box flexDirection="column" marginTop={1} marginBottom={1}>
       <Text>Installed extensions:</Text>
       <Box flexDirection="column" paddingLeft={2}>
-        {allExtensions.map((ext) => {
+        {extensions.map((ext) => {
           const state = extensionsUpdateState.get(ext.name);
           const isActive = ext.isActive;
           const activeString = isActive ? 'active' : 'disabled';

vybestack/llxprt-code

packages/a2a-server/src/commands/command-registry.ts
@@ -1,0 +1,33 @@
+/**
+ * @license
+ * Copyright 2025 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { ListExtensionsCommand } from './list-extensions.js';
+import type { Config } from '@vybestack/llxprt-code-core';
+
+export interface Command {
+  readonly names: string[];
+  execute(config: Config, args: string[]): Promise<unknown>;
+}
+
+class CommandRegistry {
+  private readonly commands = new Map<string, Command>();
+
+  constructor() {
+    this.register(new ListExtensionsCommand());
+  }
+
+  register(command: Command) {
+    for (const name of command.names) {
+      this.commands.set(name, command);
+    }
+  }
+
+  get(commandName: string): Command | undefined {
+    return this.commands.get(commandName);
+  }
+}
+
+export const commandRegistry = new CommandRegistry();

packages/a2a-server/src/commands/list-extensions.test.ts
@@ -1,0 +1,39 @@
+/**
+ * @license
+ * Copyright 2025 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { describe, it, expect, vi } from 'vitest';
+import { ListExtensionsCommand } from './list-extensions.js';
+import type { Config } from '@vybestack/llxprt-code-core';
+
+const mockListExtensions = vi.hoisted(() => vi.fn());
+vi.mock('@vybestack/llxprt-code-core', async (importOriginal) => {
+  const original =
+    await importOriginal<typeof import('@vybestack/llxprt-code-core')>();
+
+  return {
+    ...original,
+    listExtensions: mockListExtensions,
+  };
+});
+
+describe('ListExtensionsCommand', () => {
+  it('should have the correct names', () => {
+    const command = new ListExtensionsCommand();
+    expect(command.names).toEqual(['extensions', 'extensions list']);
+  });
+
+  it('should call listExtensions with the provided config', async () => {
+    const command = new ListExtensionsCommand();
+    const mockConfig = {} as Config;
+    const mockExtensions = [{ name: 'ext1' }];
+    mockListExtensions.mockReturnValue(mockExtensions);
+
+    const result = await command.execute(mockConfig, []);
+
+    expect(result).toEqual(mockExtensions);
+    expect(mockListExtensions).toHaveBeenCalledWith(mockConfig);
+  });
+});

packages/a2a-server/src/commands/list-extensions.ts
@@ -1,0 +1,16 @@
+/**
+ * @license
+ * Copyright 2025 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { listExtensions, type Config } from '@vybestack/llxprt-code-core';
+import type { Command } from './command-registry.js';
+
+export class ListExtensionsCommand implements Command {
+  readonly names = ['extensions', 'extensions list'];
+
+  async execute(config: Config, _: string[]): Promise<unknown> {
+    return listExtensions(config);
+  }
+}

packages/cli/src/ui/commands/extensionsCommand.ts
@@ -4,9 +4,10 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+import { listExtensions } from '@vybestack/llxprt-code-core';
 import type { ExtensionUpdateInfo } from '../../config/extension.js';
 import { getErrorMessage } from '../../utils/errors.js';
-import { MessageType } from '../types.js';
+import { MessageType, type HistoryItemExtensionsList } from '../types.js';
 import {
   type CommandContext,
   type SlashCommand,
@@ -14,12 +15,14 @@ import {
 } from './types.js';
 
 async function listAction(context: CommandContext) {
-  context.ui.addItem(
-    {
-      type: MessageType.EXTENSIONS_LIST,
-    },
-    Date.now(),
-  );
+  const historyItem: HistoryItemExtensionsList = {
+    type: MessageType.EXTENSIONS_LIST,
+    extensions: context.services.config
+      ? listExtensions(context.services.config)
+      : [],
+  };
+
+  context.ui.addItem(historyItem, Date.now());
 }
 
 function updateAction(context: CommandContext, args: string): Promise<void> {
@@ -42,6 +45,14 @@ function updateAction(context: CommandContext, args: string): Promise<void> {
   const updateComplete = new Promise<ExtensionUpdateInfo[]>(
     (resolve) => (resolveUpdateComplete = resolve),
   );
+
+  const historyItem: HistoryItemExtensionsList = {
+    type: MessageType.EXTENSIONS_LIST,
+    extensions: context.services.config
+      ? listExtensions(context.services.config)
+      : [],
+  };
+
   updateComplete.then((updateInfos) => {
     if (updateInfos.length === 0) {
       context.ui.addItem(
@@ -52,19 +63,13 @@ function updateAction(context: CommandContext, args: string): Promise<void> {
         Date.now(),
       );
     }
-    context.ui.addItem(
-      {
-        type: MessageType.EXTENSIONS_LIST,
-      },
-      Date.now(),
-    );
+
+    context.ui.addItem(historyItem, Date.now());
     context.ui.setPendingItem(null);
   });
 
   try {
-    context.ui.setPendingItem({
-      type: MessageType.EXTENSIONS_LIST,
-    });
+    context.ui.setPendingItem(historyItem);
 
     context.ui.dispatchExtensionStateUpdate({
       type: 'SCHEDULE_UPDATE',
@@ -77,7 +82,7 @@ function updateAction(context: CommandContext, args: string): Promise<void> {
       },
     });
     if (names?.length) {
-      const extensions = context.services.config!.getExtensions();
+      const extensions = listExtensions(context.services.config!);
       for (const name of names) {
         const extension = extensions.find(
           (extension) => extension.name === name,
@@ -120,7 +125,9 @@ const updateExtensionsCommand: SlashCommand = {
   kind: CommandKind.BUILT_IN,
   action: updateAction,
   completion: async (context, partialArg) => {
-    const extensions = context.services.config?.getExtensions() ?? [];
+    const extensions = context.services.config
+      ? listExtensions(context.services.config)
+      : [];
     const extensionNames = extensions.map((ext) => ext.name);
     const suggestions = extensionNames.filter((name) =>
       name.startsWith(partialArg),

packages/cli/src/ui/components/views/ExtensionsList.tsx
@@ -4,17 +4,22 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+import type React from 'react';
 import { Box, Text } from 'ink';
 import { useUIState } from '../../contexts/UIStateContext.js';
 import { ExtensionUpdateState } from '../../state/extensions.js';
 import { Colors } from '../../colors.js';
+import type { GeminiCLIExtension } from '@vybestack/llxprt-code-core';
 
-export const ExtensionsList = () => {
+interface ExtensionsListProps {
+  extensions: readonly GeminiCLIExtension[];
+}
+
+export const ExtensionsList: React.FC<ExtensionsListProps> = ({ extensions }) => {
   const { commandContext } = useUIState();
   const extensionsUpdateState = commandContext.ui.extensionsUpdateState;
-  const allExtensions = commandContext.services.config!.getExtensions();
 
-  if (allExtensions.length === 0) {
+  if (extensions.length === 0) {
     return <Text color={Colors.Foreground}>No extensions installed.</Text>;
   }
 
@@ -22,7 +27,7 @@ export const ExtensionsList = () => {
     <Box flexDirection="column" marginTop={1} marginBottom={1}>
       <Text color={Colors.Foreground}>Installed extensions:</Text>
       <Box flexDirection="column" paddingLeft={2}>
-        {allExtensions.map((ext) => {
+        {extensions.map((ext) => {
           const state = extensionsUpdateState.get(ext.name);
           const isActive = ext.isActive;
           const activeString = isActive ? 'active' : 'disabled';