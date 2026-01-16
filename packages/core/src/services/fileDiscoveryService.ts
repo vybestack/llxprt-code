Bare-Corporation/bare-ai-cli

fix(core): combine .gitignore and .geminiignore logic for correct precedence (#11587)
Co-authored-by: Jacob Richman <jacob314@gmail.com>

packages/core/src/services/fileDiscoveryService.ts
@@ -24,6 +24,7 @@ export interface FilterReport {
 export class FileDiscoveryService {
   private gitIgnoreFilter: GitIgnoreFilter | null = null;
   private geminiIgnoreFilter: GeminiIgnoreFilter | null = null;
+  private combinedIgnoreFilter: GitIgnoreFilter | null = null;
   private projectRoot: string;
 
   constructor(projectRoot: string) {
@@ -32,6 +33,15 @@ export class FileDiscoveryService {
       this.gitIgnoreFilter = new GitIgnoreParser(this.projectRoot);
     }
     this.geminiIgnoreFilter = new GeminiIgnoreParser(this.projectRoot);
+
+    if (this.gitIgnoreFilter) {
+      const geminiPatterns = this.geminiIgnoreFilter.getPatterns();
+      // Create combined parser: .gitignore + .geminiignore
+      this.combinedIgnoreFilter = new GitIgnoreParser(
+        this.projectRoot,
+        geminiPatterns,
+      );
+    }
   }
 
   /**
@@ -40,6 +50,14 @@ export class FileDiscoveryService {
   filterFiles(filePaths: string[], options: FilterFilesOptions = {}): string[] {
     const { respectGitIgnore = true, respectGeminiIgnore = true } = options;
     return filePaths.filter((filePath) => {
+      if (
+        respectGitIgnore &&
+        respectGeminiIgnore &&
+        this.combinedIgnoreFilter
+      ) {
+        return !this.combinedIgnoreFilter.isIgnored(filePath);
+      }
+
       if (respectGitIgnore && this.gitIgnoreFilter?.isIgnored(filePath)) {
         return false;
       }

vybestack/llxprt-code

packages/core/src/services/fileDiscoveryService.ts
@@ -27,6 +27,7 @@ export interface FilterReport {
 export class FileDiscoveryService {
   private gitIgnoreFilter: GitIgnoreFilter | null = null;
   private llxprtIgnoreFilter: GitIgnoreFilter | null = null;
+  private combinedIgnoreFilter: GitIgnoreFilter | null = null;
   private projectRoot: string;
 
   constructor(projectRoot: string) {
@@ -54,6 +55,15 @@ export class FileDiscoveryService {
       // ignore file not found
     }
     this.llxprtIgnoreFilter = gParser;
+
+    if (this.gitIgnoreFilter) {
+      const llxprtPatterns = this.llxprtIgnoreFilter.getPatterns();
+      // Create combined parser: .gitignore + .llxprtignore
+      this.combinedIgnoreFilter = new GitIgnoreParser(
+        this.projectRoot,
+        llxprtPatterns,
+      );
+    }
   }
 
   /**
@@ -66,13 +76,22 @@ export class FileDiscoveryService {
       respectLlxprtIgnore: true,
     },
   ): string[] {
+    const { respectGitIgnore = true, respectLlxprtIgnore = true } = options;
     return filePaths.filter((filePath) => {
+      if (
+        respectGitIgnore &&
+        respectLlxprtIgnore &&
+        this.combinedIgnoreFilter
+      ) {
+        return !this.combinedIgnoreFilter.isIgnored(filePath);
+      }
+
       const absolutePath = this.resolveAbsolutePath(filePath);
-      if (options.respectGitIgnore && this.shouldGitIgnoreFile(absolutePath)) {
+      if (respectGitIgnore && this.shouldGitIgnoreFile(absolutePath)) {
         return false;
       }
       if (
-        options.respectLlxprtIgnore &&
+        respectLlxprtIgnore &&
         this.shouldLlxprtIgnoreFile(absolutePath)
       ) {
         return false;