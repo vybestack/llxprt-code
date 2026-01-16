Bare-Corporation/bare-ai-cli

feat(core): Implement granular memory loaders for JIT architecture (#12195)

packages/core/src/utils/memoryDiscovery.test.ts
@@ -8,7 +8,12 @@ import { vi, describe, it, expect, beforeEach, afterEach } from 'vitest';
 import * as fsPromises from 'node:fs/promises';
 import * as os from 'node:os';
 import * as path from 'node:path';
-import { loadServerHierarchicalMemory } from './memoryDiscovery.js';
+import {
+  loadServerHierarchicalMemory,
+  loadGlobalMemory,
+  loadEnvironmentMemory,
+  loadJitSubdirectoryMemory,
+} from './memoryDiscovery.js';
 import {
   setGeminiMdFilename,
   DEFAULT_CONTEXT_FILENAME,
@@ -26,7 +31,7 @@ vi.mock('os', async (importOriginal) => {
   };
 });
 
-describe('loadServerHierarchicalMemory', () => {
+describe('memoryDiscovery', () => {
   const DEFAULT_FOLDER_TRUST = true;
   let testRootDir: string;
   let cwd: string;
@@ -611,5 +616,264 @@ included directory memory
     ).length;
     expect(parentOccurrences).toBe(1);
     expect(childOccurrences).toBe(1);
+  });
+
+  describe('loadGlobalMemory', () => {
+    it('should load global memory file if it exists', async () => {
+      const globalMemoryFile = await createTestFile(
+        path.join(homedir, GEMINI_DIR, DEFAULT_CONTEXT_FILENAME),
+        'Global memory content',
+      );
+
+      const result = await loadGlobalMemory();
+
+      expect(result.files).toHaveLength(1);
+      expect(result.files[0].path).toBe(globalMemoryFile);
+      expect(result.files[0].content).toBe('Global memory content');
+    });
+
+    it('should return empty content if global memory file does not exist', async () => {
+      const result = await loadGlobalMemory();
+
+      expect(result.files).toHaveLength(0);
+    });
+  });
+
+  describe('loadEnvironmentMemory', () => {
+    it('should load extension memory', async () => {
+      const extFile = await createTestFile(
+        path.join(testRootDir, 'ext', 'GEMINI.md'),
+        'Extension content',
+      );
+      const mockExtensionLoader = new SimpleExtensionLoader([
+        {
+          isActive: true,
+          contextFiles: [extFile],
+        } as GeminiCLIExtension,
+      ]);
+
+      const result = await loadEnvironmentMemory([], mockExtensionLoader);
+
+      expect(result.files).toHaveLength(1);
+      expect(result.files[0].path).toBe(extFile);
+      expect(result.files[0].content).toBe('Extension content');
+    });
+
+    it('should NOT traverse upward beyond trusted root (even with .git)', async () => {
+      // Setup: /temp/parent/repo/.git
+      const parentDir = await createEmptyDir(path.join(testRootDir, 'parent'));
+      const repoDir = await createEmptyDir(path.join(parentDir, 'repo'));
+      await createEmptyDir(path.join(repoDir, '.git'));
+      const srcDir = await createEmptyDir(path.join(repoDir, 'src'));
+
+      await createTestFile(
+        path.join(parentDir, DEFAULT_CONTEXT_FILENAME),
+        'Parent content',
+      );
+      await createTestFile(
+        path.join(repoDir, DEFAULT_CONTEXT_FILENAME),
+        'Repo content',
+      );
+      const srcFile = await createTestFile(
+        path.join(srcDir, DEFAULT_CONTEXT_FILENAME),
+        'Src content',
+      );
+
+      // Trust srcDir. Should ONLY load srcFile.
+      // Repo and Parent are NOT trusted.
+      const result = await loadEnvironmentMemory(
+        [srcDir],
+        new SimpleExtensionLoader([]),
+      );
+
+      expect(result.files).toHaveLength(1);
+      expect(result.files[0].path).toBe(srcFile);
+      expect(result.files[0].content).toBe('Src content');
+    });
+
+    it('should NOT traverse upward beyond trusted root (no .git)', async () => {
+      // Setup: /homedir/docs/notes (no .git anywhere)
+      const docsDir = await createEmptyDir(path.join(homedir, 'docs'));
+      const notesDir = await createEmptyDir(path.join(docsDir, 'notes'));
+
+      await createTestFile(
+        path.join(homedir, DEFAULT_CONTEXT_FILENAME),
+        'Home content',
+      );
+      const docsFile = await createTestFile(
+        path.join(docsDir, DEFAULT_CONTEXT_FILENAME),
+        'Docs content',
+      );
+
+      // Trust notesDir. Should load NOTHING because notesDir has no file,
+      // and we do not traverse up to docsDir.
+      const resultNotes = await loadEnvironmentMemory(
+        [notesDir],
+        new SimpleExtensionLoader([]),
+      );
+      expect(resultNotes.files).toHaveLength(0);
+
+      // Trust docsDir. Should load docsFile, but NOT homeFile.
+      const resultDocs = await loadEnvironmentMemory(
+        [docsDir],
+        new SimpleExtensionLoader([]),
+      );
+      expect(resultDocs.files).toHaveLength(1);
+      expect(resultDocs.files[0].path).toBe(docsFile);
+      expect(resultDocs.files[0].content).toBe('Docs content');
+    });
+
+    it('should deduplicate paths when same root is trusted multiple times', async () => {
+      const repoDir = await createEmptyDir(path.join(testRootDir, 'repo'));
+      await createEmptyDir(path.join(repoDir, '.git'));
+
+      const repoFile = await createTestFile(
+        path.join(repoDir, DEFAULT_CONTEXT_FILENAME),
+        'Repo content',
+      );
+
+      // Trust repoDir twice.
+      const result = await loadEnvironmentMemory(
+        [repoDir, repoDir],
+        new SimpleExtensionLoader([]),
+      );
+
+      expect(result.files).toHaveLength(1);
+      expect(result.files[0].path).toBe(repoFile);
+    });
+
+    it('should keep multiple memory files from the same directory adjacent and in order', async () => {
+      // Configure multiple memory filenames
+      setGeminiMdFilename(['PRIMARY.md', 'SECONDARY.md']);
+
+      const dir = await createEmptyDir(
+        path.join(testRootDir, 'multi_file_dir'),
+      );
+      await createEmptyDir(path.join(dir, '.git'));
+
+      const primaryFile = await createTestFile(
+        path.join(dir, 'PRIMARY.md'),
+        'Primary content',
+      );
+      const secondaryFile = await createTestFile(
+        path.join(dir, 'SECONDARY.md'),
+        'Secondary content',
+      );
+
+      const result = await loadEnvironmentMemory(
+        [dir],
+        new SimpleExtensionLoader([]),
+      );
+
+      expect(result.files).toHaveLength(2);
+      // Verify order: PRIMARY should come before SECONDARY because they are
+      // sorted by path and PRIMARY.md comes before SECONDARY.md alphabetically
+      // if in same dir.
+      expect(result.files[0].path).toBe(primaryFile);
+      expect(result.files[1].path).toBe(secondaryFile);
+      expect(result.files[0].content).toBe('Primary content');
+      expect(result.files[1].content).toBe('Secondary content');
+    });
+  });
+
+  describe('loadJitSubdirectoryMemory', () => {
+    it('should load JIT memory when target is inside a trusted root', async () => {
+      const rootDir = await createEmptyDir(path.join(testRootDir, 'jit_root'));
+      const subDir = await createEmptyDir(path.join(rootDir, 'subdir'));
+      const targetFile = path.join(subDir, 'target.txt');
+
+      const subDirMemory = await createTestFile(
+        path.join(subDir, DEFAULT_CONTEXT_FILENAME),
+        'Subdir JIT content',
+      );
+
+      const result = await loadJitSubdirectoryMemory(
+        targetFile,
+        [rootDir],
+        new Set(),
+      );
+
+      expect(result.files).toHaveLength(1);
+      expect(result.files[0].path).toBe(subDirMemory);
+      expect(result.files[0].content).toBe('Subdir JIT content');
+    });
+
+    it('should skip JIT memory when target is outside trusted roots', async () => {
+      const trustedRoot = await createEmptyDir(
+        path.join(testRootDir, 'trusted'),
+      );
+      const untrustedDir = await createEmptyDir(
+        path.join(testRootDir, 'untrusted'),
+      );
+      const targetFile = path.join(untrustedDir, 'target.txt');
+
+      await createTestFile(
+        path.join(untrustedDir, DEFAULT_CONTEXT_FILENAME),
+        'Untrusted content',
+      );
+
+      const result = await loadJitSubdirectoryMemory(
+        targetFile,
+        [trustedRoot],
+        new Set(),
+      );
+
+      expect(result.files).toHaveLength(0);
+    });
+
+    it('should skip already loaded paths', async () => {
+      const rootDir = await createEmptyDir(path.join(testRootDir, 'jit_root'));
+      const subDir = await createEmptyDir(path.join(rootDir, 'subdir'));
+      const targetFile = path.join(subDir, 'target.txt');
+
+      const rootMemory = await createTestFile(
+        path.join(rootDir, DEFAULT_CONTEXT_FILENAME),
+        'Root content',
+      );
+      const subDirMemory = await createTestFile(
+        path.join(subDir, DEFAULT_CONTEXT_FILENAME),
+        'Subdir content',
+      );
+
+      // Simulate root memory already loaded (e.g., by loadEnvironmentMemory)
+      const alreadyLoaded = new Set([rootMemory]);
+
+      const result = await loadJitSubdirectoryMemory(
+        targetFile,
+        [rootDir],
+        alreadyLoaded,
+      );
+
+      expect(result.files).toHaveLength(1);
+      expect(result.files[0].path).toBe(subDirMemory);
+      expect(result.files[0].content).toBe('Subdir content');
+    });
+
+    it('should use the deepest trusted root when multiple nested roots exist', async () => {
+      const outerRoot = await createEmptyDir(path.join(testRootDir, 'outer'));
+      const innerRoot = await createEmptyDir(path.join(outerRoot, 'inner'));
+      const targetFile = path.join(innerRoot, 'target.txt');
+
+      const outerMemory = await createTestFile(
+        path.join(outerRoot, DEFAULT_CONTEXT_FILENAME),
+        'Outer content',
+      );
+      const innerMemory = await createTestFile(
+        path.join(innerRoot, DEFAULT_CONTEXT_FILENAME),
+        'Inner content',
+      );
+
+      const result = await loadJitSubdirectoryMemory(
+        targetFile,
+        [outerRoot, innerRoot],
+        new Set(),
+      );
+
+      expect(result.files).toHaveLength(1);
+      expect(result.files[0].path).toBe(innerMemory);
+      expect(result.files[0].content).toBe('Inner content');
+      // Ensure outer memory is NOT loaded
+      expect(result.files.find((f) => f.path === outerMemory)).toBeUndefined();
+    });
   });
 });

vybestack/llxprt-code

packages/core/src/utils/memoryDiscovery.test.ts
@@ -5,10 +5,15 @@
  */
 
 import { vi, describe, it, expect, beforeEach, afterEach } from 'vitest';
-import * as fsPromises from 'fs/promises';
-import * as os from 'os';
-import * as path from 'path';
-import { loadServerHierarchicalMemory } from './memoryDiscovery.js';
+import * as fsPromises from 'node:fs/promises';
+import * as os from 'node:os';
+import * as path from 'node:path';
+import {
+  loadServerHierarchicalMemory,
+  loadGlobalMemory,
+  loadEnvironmentMemory,
+  loadJitSubdirectoryMemory,
+} from './memoryDiscovery.js';
 import {
   setLlxprtMdFilename,
   DEFAULT_CONTEXT_FILENAME,
@@ -25,7 +30,15 @@ vi.mock('os', async (importOriginal) => {
   };
 });
 
-describe('loadServerHierarchicalMemory', () => {
+// Simple extension loader for testing
+class SimpleExtensionLoader {
+  constructor(private extensions: GeminiCLIExtension[]) {}
+  getExtensions() {
+    return this.extensions;
+  }
+}
+
+describe('memoryDiscovery', () => {
   const DEFAULT_FOLDER_TRUST = true;
   let testRootDir: string;
   let cwd: string;
@@ -698,6 +711,265 @@ included directory memory
         (p) => p === globalFile,
       ).length;
       expect(occurrences).toBe(1);
+    });
+  });
+
+  describe('loadGlobalMemory', () => {
+    it('should load global memory file if it exists', async () => {
+      const globalMemoryFile = await createTestFile(
+        path.join(homedir, LLXPRT_DIR, DEFAULT_CONTEXT_FILENAME),
+        'Global memory content',
+      );
+
+      const result = await loadGlobalMemory();
+
+      expect(result.files).toHaveLength(1);
+      expect(result.files[0].path).toBe(globalMemoryFile);
+      expect(result.files[0].content).toBe('Global memory content');
+    });
+
+    it('should return empty content if global memory file does not exist', async () => {
+      const result = await loadGlobalMemory();
+
+      expect(result.files).toHaveLength(0);
+    });
+  });
+
+  describe('loadEnvironmentMemory', () => {
+    it('should load extension memory', async () => {
+      const extFile = await createTestFile(
+        path.join(testRootDir, 'ext', 'LLXPRT.md'),
+        'Extension content',
+      );
+      const mockExtensionLoader = new SimpleExtensionLoader([
+        {
+          isActive: true,
+          contextFiles: [extFile],
+        } as GeminiCLIExtension,
+      ]);
+
+      const result = await loadEnvironmentMemory([], mockExtensionLoader);
+
+      expect(result.files).toHaveLength(1);
+      expect(result.files[0].path).toBe(extFile);
+      expect(result.files[0].content).toBe('Extension content');
+    });
+
+    it('should NOT traverse upward beyond trusted root (even with .git)', async () => {
+      // Setup: /temp/parent/repo/.git
+      const parentDir = await createEmptyDir(path.join(testRootDir, 'parent'));
+      const repoDir = await createEmptyDir(path.join(parentDir, 'repo'));
+      await createEmptyDir(path.join(repoDir, '.git'));
+      const srcDir = await createEmptyDir(path.join(repoDir, 'src'));
+
+      await createTestFile(
+        path.join(parentDir, DEFAULT_CONTEXT_FILENAME),
+        'Parent content',
+      );
+      await createTestFile(
+        path.join(repoDir, DEFAULT_CONTEXT_FILENAME),
+        'Repo content',
+      );
+      const srcFile = await createTestFile(
+        path.join(srcDir, DEFAULT_CONTEXT_FILENAME),
+        'Src content',
+      );
+
+      // Trust srcDir. Should ONLY load srcFile.
+      // Repo and Parent are NOT trusted.
+      const result = await loadEnvironmentMemory(
+        [srcDir],
+        new SimpleExtensionLoader([]),
+      );
+
+      expect(result.files).toHaveLength(1);
+      expect(result.files[0].path).toBe(srcFile);
+      expect(result.files[0].content).toBe('Src content');
+    });
+
+    it('should NOT traverse upward beyond trusted root (no .git)', async () => {
+      // Setup: /homedir/docs/notes (no .git anywhere)
+      const docsDir = await createEmptyDir(path.join(homedir, 'docs'));
+      const notesDir = await createEmptyDir(path.join(docsDir, 'notes'));
+
+      await createTestFile(
+        path.join(homedir, DEFAULT_CONTEXT_FILENAME),
+        'Home content',
+      );
+      const docsFile = await createTestFile(
+        path.join(docsDir, DEFAULT_CONTEXT_FILENAME),
+        'Docs content',
+      );
+
+      // Trust notesDir. Should load NOTHING because notesDir has no file,
+      // and we do not traverse up to docsDir.
+      const resultNotes = await loadEnvironmentMemory(
+        [notesDir],
+        new SimpleExtensionLoader([]),
+      );
+      expect(resultNotes.files).toHaveLength(0);
+
+      // Trust docsDir. Should load docsFile, but NOT homeFile.
+      const resultDocs = await loadEnvironmentMemory(
+        [docsDir],
+        new SimpleExtensionLoader([]),
+      );
+      expect(resultDocs.files).toHaveLength(1);
+      expect(resultDocs.files[0].path).toBe(docsFile);
+      expect(resultDocs.files[0].content).toBe('Docs content');
+    });
+
+    it('should deduplicate paths when same root is trusted multiple times', async () => {
+      const repoDir = await createEmptyDir(path.join(testRootDir, 'repo'));
+      await createEmptyDir(path.join(repoDir, '.git'));
+
+      const repoFile = await createTestFile(
+        path.join(repoDir, DEFAULT_CONTEXT_FILENAME),
+        'Repo content',
+      );
+
+      // Trust repoDir twice.
+      const result = await loadEnvironmentMemory(
+        [repoDir, repoDir],
+        new SimpleExtensionLoader([]),
+      );
+
+      expect(result.files).toHaveLength(1);
+      expect(result.files[0].path).toBe(repoFile);
+    });
+
+    it('should keep multiple memory files from the same directory adjacent and in order', async () => {
+      // Configure multiple memory filenames
+      setLlxprtMdFilename(['PRIMARY.md', 'SECONDARY.md']);
+
+      const dir = await createEmptyDir(
+        path.join(testRootDir, 'multi_file_dir'),
+      );
+      await createEmptyDir(path.join(dir, '.git'));
+
+      const primaryFile = await createTestFile(
+        path.join(dir, 'PRIMARY.md'),
+        'Primary content',
+      );
+      const secondaryFile = await createTestFile(
+        path.join(dir, 'SECONDARY.md'),
+        'Secondary content',
+      );
+
+      const result = await loadEnvironmentMemory(
+        [dir],
+        new SimpleExtensionLoader([]),
+      );
+
+      expect(result.files).toHaveLength(2);
+      // Verify order: PRIMARY should come before SECONDARY because they are
+      // sorted by path and PRIMARY.md comes before SECONDARY.md alphabetically
+      // if in same dir.
+      expect(result.files[0].path).toBe(primaryFile);
+      expect(result.files[1].path).toBe(secondaryFile);
+      expect(result.files[0].content).toBe('Primary content');
+      expect(result.files[1].content).toBe('Secondary content');
+    });
+  });
+
+  describe('loadJitSubdirectoryMemory', () => {
+    it('should load JIT memory when target is inside a trusted root', async () => {
+      const rootDir = await createEmptyDir(path.join(testRootDir, 'jit_root'));
+      const subDir = await createEmptyDir(path.join(rootDir, 'subdir'));
+      const targetFile = path.join(subDir, 'target.txt');
+
+      const subDirMemory = await createTestFile(
+        path.join(subDir, DEFAULT_CONTEXT_FILENAME),
+        'Subdir JIT content',
+      );
+
+      const result = await loadJitSubdirectoryMemory(
+        targetFile,
+        [rootDir],
+        new Set(),
+      );
+
+      expect(result.files).toHaveLength(1);
+      expect(result.files[0].path).toBe(subDirMemory);
+      expect(result.files[0].content).toBe('Subdir JIT content');
+    });
+
+    it('should skip JIT memory when target is outside trusted roots', async () => {
+      const trustedRoot = await createEmptyDir(
+        path.join(testRootDir, 'trusted'),
+      );
+      const untrustedDir = await createEmptyDir(
+        path.join(testRootDir, 'untrusted'),
+      );
+      const targetFile = path.join(untrustedDir, 'target.txt');
+
+      await createTestFile(
+        path.join(untrustedDir, DEFAULT_CONTEXT_FILENAME),
+        'Untrusted content',
+      );
+
+      const result = await loadJitSubdirectoryMemory(
+        targetFile,
+        [trustedRoot],
+        new Set(),
+      );
+
+      expect(result.files).toHaveLength(0);
+    });
+
+    it('should skip already loaded paths', async () => {
+      const rootDir = await createEmptyDir(path.join(testRootDir, 'jit_root'));
+      const subDir = await createEmptyDir(path.join(rootDir, 'subdir'));
+      const targetFile = path.join(subDir, 'target.txt');
+
+      const rootMemory = await createTestFile(
+        path.join(rootDir, DEFAULT_CONTEXT_FILENAME),
+        'Root content',
+      );
+      const subDirMemory = await createTestFile(
+        path.join(subDir, DEFAULT_CONTEXT_FILENAME),
+        'Subdir content',
+      );
+
+      // Simulate root memory already loaded (e.g., by loadEnvironmentMemory)
+      const alreadyLoaded = new Set([rootMemory]);
+
+      const result = await loadJitSubdirectoryMemory(
+        targetFile,
+        [rootDir],
+        alreadyLoaded,
+      );
+
+      expect(result.files).toHaveLength(1);
+      expect(result.files[0].path).toBe(subDirMemory);
+      expect(result.files[0].content).toBe('Subdir content');
+    });
+
+    it('should use the deepest trusted root when multiple nested roots exist', async () => {
+      const outerRoot = await createEmptyDir(path.join(testRootDir, 'outer'));
+      const innerRoot = await createEmptyDir(path.join(outerRoot, 'inner'));
+      const targetFile = path.join(innerRoot, 'target.txt');
+
+      const outerMemory = await createTestFile(
+        path.join(outerRoot, DEFAULT_CONTEXT_FILENAME),
+        'Outer content',
+      );
+      const innerMemory = await createTestFile(
+        path.join(innerRoot, DEFAULT_CONTEXT_FILENAME),
+        'Inner content',
+      );
+
+      const result = await loadJitSubdirectoryMemory(
+        targetFile,
+        [outerRoot, innerRoot],
+        new Set(),
+      );
+
+      expect(result.files).toHaveLength(1);
+      expect(result.files[0].path).toBe(innerMemory);
+      expect(result.files[0].content).toBe('Inner content');
+      // Ensure outer memory is NOT loaded
+      expect(result.files.find((f) => f.path === outerMemory)).toBeUndefined();
     });
   });
 });