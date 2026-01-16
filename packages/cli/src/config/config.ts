Bare-Corporation/bare-ai-cli

Initial support for reloading extensions in the CLI - mcp servers only (#12239)

packages/cli/src/config/config.ts
@@ -680,6 +680,7 @@ export async function loadCliConfig(
     listExtensions: argv.listExtensions || false,
     enabledExtensions: argv.extensions,
     extensionLoader: extensionManager,
+    enableExtensionReloading: settings.experimental?.extensionReloading,
     blockedMcpServers,
     noBrowser: !!process.env['NO_BROWSER'],
     summarizeToolOutput: settings.model?.summarizeToolOutput,

vybestack/llxprt-code

packages/cli/src/config/config.ts
@@ -1362,6 +1362,7 @@ export async function loadCliConfig(
     })),
     provider: finalProvider,
     extensions: allExtensions,
+    enableExtensionReloading: effectiveSettings.experimental?.extensionReloading,
     blockedMcpServers,
     noBrowser: !!process.env.NO_BROWSER,
     summarizeToolOutput: effectiveSettings.summarizeToolOutput,