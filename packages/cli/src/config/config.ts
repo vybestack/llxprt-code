Bare-Corporation/bare-ai-cli

Agent Skills: Implement Core Skill Infrastructure & Tiered Discovery (#15698)

packages/cli/src/config/config.ts
@@ -667,6 +667,8 @@ export async function loadCliConfig(
     extensionLoader: extensionManager,
     enableExtensionReloading: settings.experimental?.extensionReloading,
     enableAgents: settings.experimental?.enableAgents,
+    skillsSupport: settings.experimental?.skills,
+    disabledSkills: settings.skills?.disabled,
     experimentalJitContext: settings.experimental?.jitContext,
     noBrowser: !!process.env['NO_BROWSER'],
     summarizeToolOutput: settings.model?.summarizeToolOutput,

packages/core/src/config/storage.test.ts
@@ -45,6 +45,16 @@ describe('Storage – additional helpers', () => {
     expect(storage.getProjectCommandsDir()).toBe(expected);
   });
 
+  it('getUserSkillsDir returns ~/.gemini/skills', () => {
+    const expected = path.join(os.homedir(), GEMINI_DIR, 'skills');
+    expect(Storage.getUserSkillsDir()).toBe(expected);
+  });
+
+  it('getProjectSkillsDir returns project/.gemini/skills', () => {
+    const expected = path.join(projectRoot, GEMINI_DIR, 'skills');
+    expect(storage.getProjectSkillsDir()).toBe(expected);
+  });
+
   it('getUserAgentsDir returns ~/.gemini/agents', () => {
     const expected = path.join(os.homedir(), GEMINI_DIR, 'agents');
     expect(Storage.getUserAgentsDir()).toBe(expected);

packages/core/src/config/storage.ts
@@ -50,6 +50,10 @@ export class Storage {
     return path.join(Storage.getGlobalGeminiDir(), 'commands');
   }
 
+  static getUserSkillsDir(): string {
+    return path.join(Storage.getGlobalGeminiDir(), 'skills');
+  }
+
   static getGlobalMemoryFilePath(): string {
     return path.join(Storage.getGlobalGeminiDir(), 'memory.md');
   }
@@ -125,6 +129,10 @@ export class Storage {
 
   getProjectCommandsDir(): string {
     return path.join(this.getGeminiDir(), 'commands');
+  }
+
+  getProjectSkillsDir(): string {
+    return path.join(this.getGeminiDir(), 'skills');
   }
 
   getProjectAgentsDir(): string {

vybestack/llxprt-code

packages/cli/src/config/config.ts
@@ -1423,6 +1423,8 @@ export async function loadCliConfig(
     maxSessionTurns: effectiveSettings.ui?.maxSessionTurns ?? -1,
     experimentalZedIntegration: argv.experimentalAcp || false,
     listExtensions: argv.listExtensions || false,
+    listSessions: argv.listSessions || false,
+    deleteSession: argv.deleteSession,
     activeExtensions: activeExtensions.map((e) => ({
       name: e.name,
       version: e.version,
@@ -1432,6 +1434,8 @@ export async function loadCliConfig(
     enableExtensionReloading:
       effectiveSettings.experimental?.extensionReloading,
     blockedMcpServers,
+    skillsSupport: effectiveSettings.experimental?.skills,
+    disabledSkills: effectiveSettings.skills?.disabled,
     noBrowser: !!process.env.NO_BROWSER,
     summarizeToolOutput: effectiveSettings.summarizeToolOutput,
     ideMode,

packages/core/src/config/storage.test.ts
@@ -44,7 +44,13 @@ describe('Storage – additional helpers', () => {
     expect(storage.getProjectCommandsDir()).toBe(expected);
   });
 
+  it('getProjectSkillsDir returns project/.llxprt/skills', () => {
+    const expected = path.join(projectRoot, '.llxprt', 'skills');
+    expect(storage.getProjectSkillsDir()).toBe(expected);
+  });
+
   it('getMcpOAuthTokensPath returns ~/.llxprt/mcp-oauth-tokens.json', () => {
+>>>>>>> de1233b8c (Agent Skills: Implement Core Skill Infrastructure & Tiered Discovery (#15698))
     const expected = path.join(
       os.homedir(),
       '.llxprt',

packages/core/src/config/storage.ts
@@ -53,6 +53,10 @@ export class Storage {
     return path.join(Storage.getGlobalLlxprtDir(), 'commands');
   }
 
+  static getUserSkillsDir(): string {
+    return path.join(Storage.getGlobalGeminiDir(), 'skills');
+  }
+
   static getGlobalMemoryFilePath(): string {
     return path.join(Storage.getGlobalLlxprtDir(), 'memory.md');
   }
@@ -120,6 +124,10 @@ export class Storage {
 
   getProjectCommandsDir(): string {
     return path.join(this.getLlxprtDir(), 'commands');
+  }
+
+  getProjectSkillsDir(): string {
+    return path.join(this.getLlxprtDir(), 'skills');
   }
 
   getProjectTempCheckpointsDir(): string {