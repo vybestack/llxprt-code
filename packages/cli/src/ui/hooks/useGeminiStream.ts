Bare-Corporation/bare-ai-cli

Fix race condition by awaiting scheduleToolCalls (#16759)
Co-authored-by: Tommaso Sciortino <sciortino@gmail.com>

packages/cli/src/ui/hooks/useGeminiStream.ts
@@ -460,7 +460,7 @@ export const useGeminiStream = (
                   isClientInitiated: true,
                   prompt_id,
                 };
-                scheduleToolCalls([toolCallRequest], abortSignal);
+                await scheduleToolCalls([toolCallRequest], abortSignal);
                 return { queryToSend: null, shouldProceed: false };
               }
               case 'submit_prompt': {
@@ -923,7 +923,7 @@ export const useGeminiStream = (
         }
       }
       if (toolCallRequests.length > 0) {
-        scheduleToolCalls(toolCallRequests, signal);
+        await scheduleToolCalls(toolCallRequests, signal);
       }
       return StreamProcessingStatus.Completed;
     },

vybestack/llxprt-code

packages/cli/src/ui/hooks/useGeminiStream.ts
@@ -620,7 +620,7 @@ export const useGeminiStream = (
                   prompt_id,
                   agentId: DEFAULT_AGENT_ID,
                 };
-                scheduleToolCalls([toolCallRequest], abortSignal);
+                await scheduleToolCalls([toolCallRequest], abortSignal);
                 return { queryToSend: null, shouldProceed: false };
               }
               case 'submit_prompt': {
@@ -1190,7 +1190,7 @@ export const useGeminiStream = (
         });
 
         if (dedupedToolCallRequests.length > 0) {
-          scheduleToolCalls(dedupedToolCallRequests, signal);
+          await scheduleToolCalls(dedupedToolCallRequests, signal);
         }
       }
       return StreamProcessingStatus.Completed;