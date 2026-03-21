b46303b74b6127c3b90226db935d8960ea30ad21
acoliver <acoliver@gmail.com>
2026-03-21 15:22:08 -0300

    cherry-pick: upstream 57012ae5b33 batch 06
    Adds Rewind data structure fields (filePath, isNewFile) to FileDiff and tools.
    Fixes cherry-pick type errors and test expectations for LLxprt field names.

--- packages/core/src/core/client.test.ts
+++ packages/core/src/core/client.test.ts
@@ -1083,6 +1083,7 @@ sub memory
     });
   });
 
+
   describe('sendMessageStream', () => {
     beforeEach(() => {
       (
--- packages/core/src/core/coreToolScheduler.ts
+++ packages/core/src/core/coreToolScheduler.ts
@@ -507,6 +507,7 @@ export class CoreToolScheduler {
                 originalContent:
                   waitingCall.confirmationDetails.originalContent,
                 newContent: waitingCall.confirmationDetails.newContent,
+                filePath: waitingCall.confirmationDetails.filePath,
               };
             }
           }
